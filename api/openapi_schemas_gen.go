e4ef3dc207b4b53a9a65abd591810393cd7af29c
rs <yuiop1955@mail.ru>
2021-09-21 09:40:55 +0500

    chore: go generate

--- api/openapi_schemas_gen.go
+++ api/openapi_schemas_gen.go
@@ -57,10 +57,10 @@ func (*NotFound) foobarGetResponse()  {}
 func (*NotFound) foobarPostResponse() {}
 
 type Pet struct {
-	ID       int64     `json:"id"`
-	Name     string    `json:"name"`
 	Birthday time.Time `json:"birthday"`
 	Friends  []Pet     `json:"friends"`
+	ID       int64     `json:"id"`
+	Name     string    `json:"name"`
 }
 
 func (*Pet) foobarGetResponse()  {}
