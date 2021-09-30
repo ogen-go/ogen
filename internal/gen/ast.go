f96eb16bd99b68f1ef3ed552796d23d6f9359a2a
rs <yuiop1955@mail.ru>
2021-09-30 21:21:54 +0500

    fix: make response encoder generation deterministic

--- internal/gen/ast.go
+++ internal/gen/ast.go
@@ -2,6 +2,7 @@ package gen
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
 
@@ -247,43 +248,61 @@ func (r *Response) unimplement(iface *Interface) {
 }
 
 type ResponseInfo struct {
+	Schema      *Schema
 	StatusCode  int
+	Default     bool
 	ContentType string
 	NoContent   bool
-	Default     bool
 }
 
-func (m *Method) ListResponseSchemas() map[*Schema]ResponseInfo {
-	schemas := make(map[*Schema]ResponseInfo)
+func (m *Method) ListResponseSchemas() []ResponseInfo {
+	var result []ResponseInfo
 	for statusCode, resp := range m.Responses {
 		if resp.NoContent != nil {
-			schemas[resp.NoContent] = ResponseInfo{
+			result = append(result, ResponseInfo{
+				Schema:     resp.NoContent,
 				StatusCode: statusCode,
 				NoContent:  true,
-			}
+			})
 			continue
 		}
 		for contentType, schema := range resp.Contents {
-			schemas[schema] = ResponseInfo{
+			result = append(result, ResponseInfo{
+				Schema:      schema,
 				StatusCode:  statusCode,
 				ContentType: contentType,
-			}
+			})
 		}
 	}
 
 	if def := m.ResponseDefault; def != nil {
 		if noc := def.NoContent; noc != nil {
-			schemas[noc] = ResponseInfo{
+			result = append(result, ResponseInfo{
+				Schema:    noc,
 				Default:   true,
 				NoContent: true,
-			}
+			})
 		}
 		for contentType, schema := range def.Contents {
-			schemas[schema] = ResponseInfo{
+			result = append(result, ResponseInfo{
+				Schema:      schema,
 				Default:     true,
 				ContentType: contentType,
-			}
+			})
 		}
 	}
-	return schemas
+
+	sort.SliceStable(result, func(i, j int) bool {
+		l, r := result[i], result[j]
+		// Default responses has zero status code.
+		if l.Default {
+			l.StatusCode = 999
+		}
+		if r.Default {
+			r.StatusCode = 999
+		}
+		return l.StatusCode < r.StatusCode
+	})
+
+	return result
 }
