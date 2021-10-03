385bdba8d913123a29bc5caffa26aa342009cbdb
Aleksandr Razumov <ernado@ya.ru>
2021-10-03 19:48:52 +0300

    test: add json TechEmpower test/bench

--- bench_test.go
+++ bench_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/require"
 
 	"github.com/ogen-go/ogen/internal/techempower"
 )
@@ -18,11 +19,10 @@ func (t techEmpowerServer) JSON(ctx context.Context) (*techempower.HelloWorld, e
 	}, nil
 }
 
-func BenchmarkIntegration(b *testing.B) {
+func TestIntegration(t *testing.T) {
 	// Using TechEmpower as most popular general purpose framework benchmark.
-	b.Run("TechEmpower", func(b *testing.B) {
-		b.ReportAllocs()
-
+	// https://github.com/TechEmpower/FrameworkBenchmarks/wiki/Project-Information-Framework-Tests-Overview#test-types
+	t.Run("TechEmpower", func(t *testing.T) {
 		mux := chi.NewRouter()
 		techempower.Register(mux, techEmpowerServer{})
 		s := httptest.NewServer(mux)
@@ -31,18 +31,41 @@ func BenchmarkIntegration(b *testing.B) {
 		client := techempower.NewClient(s.URL)
 		ctx := context.Background()
 
-		b.ResetTimer()
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				hw, err := client.JSON(ctx)
-				if err != nil {
-					b.Error(err)
-					return
+		t.Run("JSON", func(t *testing.T) {
+			res, err := client.JSON(ctx)
+			require.NoError(t, err)
+			require.Equal(t, "Hello, world!", res.Message)
+		})
+	})
+}
+
+func BenchmarkIntegration(b *testing.B) {
+	// Using TechEmpower as most popular general purpose framework benchmark.
+	// https://github.com/TechEmpower/FrameworkBenchmarks/wiki/Project-Information-Framework-Tests-Overview#test-types
+	b.Run("TechEmpower", func(b *testing.B) {
+		mux := chi.NewRouter()
+		techempower.Register(mux, techEmpowerServer{})
+		s := httptest.NewServer(mux)
+		defer s.Close()
+
+		client := techempower.NewClient(s.URL)
+		ctx := context.Background()
+
+		b.Run("JSON", func(b *testing.B) {
+			b.ReportAllocs()
+			b.ResetTimer()
+			b.RunParallel(func(pb *testing.PB) {
+				for pb.Next() {
+					hw, err := client.JSON(ctx)
+					if err != nil {
+						b.Error(err)
+						return
+					}
+					if hw.Message != "Hello, world!" {
+						b.Error("mismatch")
+					}
 				}
-				if hw.Message != "Hello, world!" {
-					b.Error("mismatch")
-				}
-			}
+			})
 		})
 	})
 }
