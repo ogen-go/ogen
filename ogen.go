3185eed447821d753ec561b7a59619ec99fe1056
Aleksandr Razumov <ernado@ya.ru>
2021-10-04 08:52:31 +0300

    refactor: move sample api to internal

--- ogen.go
+++ ogen.go
@@ -1,5 +1,5 @@
 // Package ogen implements OpenAPI v3 code generation.
 package ogen
 
-//go:generate go run ./cmd/ogen --schema _testdata/sample_1.json --clean
+//go:generate go run ./cmd/ogen --schema _testdata/sample_1.json --target internal/sample_api --clean
 //go:generate go run ./cmd/ogen --schema _testdata/techempower.json --target internal/techempower --package techempower --clean
