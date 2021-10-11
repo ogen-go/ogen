df1f6ff63ea568b26df188e17b575276f4b125bc
rs <yuiop1955@mail.ru>
2021-10-12 01:37:45 +0500

    chore(uri): add missing path encoder methods

--- uri/path_encoder.go
+++ uri/path_encoder.go
@@ -97,6 +97,22 @@ func (e *PathEncoder) EncodeInt64(v int64) string {
 	return e.EncodeString(strconv.FormatInt(v, 10))
 }
 
+func (e *PathEncoder) EncodeInt32(v int32) string {
+	return e.EncodeInt64(int64(v))
+}
+
+func (e *PathEncoder) EncodeInt(v int) string {
+	return e.EncodeInt64(int64(v))
+}
+
+func (e *PathEncoder) EncodeFloat64(v float64) string {
+	return e.EncodeString(strconv.FormatFloat(v, 'f', 10, 64))
+}
+
+func (e *PathEncoder) EncodeFloat32(v float32) string {
+	return e.EncodeFloat64(float64(v))
+}
+
 func (e *PathEncoder) EncodeBool(v bool) string {
 	return e.EncodeString(strconv.FormatBool(v))
 }
@@ -105,6 +121,38 @@ func (e *PathEncoder) EncodeInt64Array(vs []int64) string {
 	strs := make([]string, 0, len(vs))
 	for _, v := range vs {
 		strs = append(strs, strconv.FormatInt(v, 10))
+	}
+	return e.EncodeStringArray(strs)
+}
+
+func (e *PathEncoder) EncodeInt32Array(vs []int32) string {
+	strs := make([]string, 0, len(vs))
+	for _, v := range vs {
+		strs = append(strs, strconv.FormatInt(int64(v), 10))
+	}
+	return e.EncodeStringArray(strs)
+}
+
+func (e *PathEncoder) EncodeIntArray(vs []int) string {
+	strs := make([]string, 0, len(vs))
+	for _, v := range vs {
+		strs = append(strs, strconv.FormatInt(int64(v), 10))
+	}
+	return e.EncodeStringArray(strs)
+}
+
+func (e *PathEncoder) EncodeFloat64Array(vs []float64) string {
+	strs := make([]string, 0, len(vs))
+	for _, v := range vs {
+		strs = append(strs, strconv.FormatFloat(v, 'f', 10, 64))
+	}
+	return e.EncodeStringArray(strs)
+}
+
+func (e *PathEncoder) EncodeFloat32Array(vs []float32) string {
+	strs := make([]string, 0, len(vs))
+	for _, v := range vs {
+		strs = append(strs, strconv.FormatFloat(float64(v), 'f', 10, 64))
 	}
 	return e.EncodeStringArray(strs)
 }
