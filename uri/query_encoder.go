3280f4c01d26423ccecf823f0bc5297db7137a1e
rs <yuiop1955@mail.ru>
2021-10-12 01:31:13 +0500

    chore(uri): add missing query encoder methods

--- uri/query_encoder.go
+++ uri/query_encoder.go
@@ -68,29 +68,27 @@ func (e *QueryEncoder) EncodeStringArray(vs []string) []string {
 }
 
 func (e *QueryEncoder) EncodeBool(v bool) string {
-	switch e.style {
-	case QueryStyleForm:
-		return strconv.FormatBool(v)
-	case QueryStyleSpaceDelimited,
-		QueryStylePipeDelimited,
-		QueryStyleDeepObject:
-		panic(fmt.Sprintf("style '%s' cannot be used for primitive values", e.style))
-	default:
-		panic("unreachable")
-	}
+	return e.EncodeString(strconv.FormatBool(v))
 }
 
 func (e *QueryEncoder) EncodeInt64(v int64) string {
-	switch e.style {
-	case QueryStyleForm:
-		return strconv.FormatInt(v, 10)
-	case QueryStyleSpaceDelimited,
-		QueryStylePipeDelimited,
-		QueryStyleDeepObject:
-		panic(fmt.Sprintf("style '%s' cannot be used for primitive values", e.style))
-	default:
-		panic("unreachable")
-	}
+	return e.EncodeString(strconv.FormatInt(v, 10))
+}
+
+func (e *QueryEncoder) EncodeInt32(v int32) string {
+	return e.EncodeInt64(int64(v))
+}
+
+func (e *QueryEncoder) EncodeInt(v int) string {
+	return e.EncodeInt64(int64(v))
+}
+
+func (e *QueryEncoder) EncodeFloat64(v float64) string {
+	return e.EncodeString(strconv.FormatFloat(v, 'f', 10, 64))
+}
+
+func (e *QueryEncoder) EncodeFloat32(v float32) string {
+	return e.EncodeFloat64(float64(v))
 }
 
 func (e *QueryEncoder) EncodeBoolArray(vs []bool) []string {
@@ -105,6 +103,38 @@ func (e *QueryEncoder) EncodeInt64Array(vs []int64) []string {
 	strs := make([]string, 0, len(vs))
 	for _, v := range vs {
 		strs = append(strs, strconv.FormatInt(v, 10))
+	}
+	return e.EncodeStringArray(strs)
+}
+
+func (e *QueryEncoder) EncodeInt32Array(vs []int32) []string {
+	strs := make([]string, 0, len(vs))
+	for _, v := range vs {
+		strs = append(strs, strconv.FormatInt(int64(v), 10))
+	}
+	return e.EncodeStringArray(strs)
+}
+
+func (e *QueryEncoder) EncodeIntArray(vs []int) []string {
+	strs := make([]string, 0, len(vs))
+	for _, v := range vs {
+		strs = append(strs, strconv.FormatInt(int64(v), 10))
+	}
+	return e.EncodeStringArray(strs)
+}
+
+func (e *QueryEncoder) EncodeFloat64Array(vs []float64) []string {
+	strs := make([]string, 0, len(vs))
+	for _, v := range vs {
+		strs = append(strs, strconv.FormatFloat(v, 'f', 10, 64))
+	}
+	return e.EncodeStringArray(strs)
+}
+
+func (e *QueryEncoder) EncodeFloat32Array(vs []float32) []string {
+	strs := make([]string, 0, len(vs))
+	for _, v := range vs {
+		strs = append(strs, strconv.FormatFloat(float64(v), 'f', 10, 64))
 	}
 	return e.EncodeStringArray(strs)
 }
