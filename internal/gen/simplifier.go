8b274a88b832fcd3b5550ff7a22ff1431e96c7b5
Aleksandr Razumov <ernado@ya.ru>
2021-10-17 15:15:06 +0300

    chore(gen): simplify condition

--- internal/gen/simplifier.go
+++ internal/gen/simplifier.go
@@ -81,7 +81,7 @@ func (g *Generator) devirtSingleResponse(m *ast.Method) {
 }
 
 func (g *Generator) devirtDefaultResponse(m *ast.Method) {
-	if ok := (m.Responses.Default != nil && len(m.Responses.StatusCode) == 0); !ok {
+	if !(m.Responses.Default != nil && len(m.Responses.StatusCode) == 0) {
 		return
 	}
 
