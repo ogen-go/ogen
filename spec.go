31858798941f3a5a300d428df2e80e2228dada36
Aleksandr Razumov <ernado@ya.ru>
2021-10-24 00:15:42 +0300

    feat: omitempties

--- spec.go
+++ spec.go
@@ -10,7 +10,7 @@ type Spec struct {
 	Info       Info        `json:"info"`
 	Servers    []Server    `json:"servers"`
 	Paths      Paths       `json:"paths"`
-	Components *Components `json:"components"`
+	Components *Components `json:"components,omitempty"`
 }
 
 // Init components of schema.
@@ -47,9 +47,9 @@ type Info struct {
 	// A URL to the Terms of Service for the API. MUST be in the format of a URL.
 	TermsOfService string `json:"termsOfService"`
 	// The contact information for the exposed API.
-	Contact *Contact `json:"contact"`
+	Contact *Contact `json:"contact,omitempty"`
 	// The license information for the exposed API.
-	License *License `json:"license"`
+	License *License `json:"license,omitempty"`
 	// REQUIRED. The version of the OpenAPI document.
 	Version string `json:"version"`
 }
@@ -77,11 +77,11 @@ type Server struct {
 // All objects defined within the components object will have no effect on the API
 // unless they are explicitly referenced from properties outside the components object.
 type Components struct {
-	Schemas    map[string]Schema    `json:"schemas"`
-	Responses  map[string]Response  `json:"responses"`
+	Schemas    map[string]Schema    `json:"schemas,omitempty"`
+	Responses  map[string]Response  `json:"responses,omitempty"`
 	Parameters map[string]Parameter `json:"parameters"`
 	// Examples        map[string]Example         `json:"example"`
-	RequestBodies map[string]RequestBody `json:"requestBodies"`
+	RequestBodies map[string]RequestBody `json:"requestBodies,omitempty"`
 	// Headers         map[string]Header          `json:"headers"`
 	// SecuritySchemes map[string]SecuritySchema  `json:"securitySchemes"`
 	// Links           map[string]Link            `json:"links"`
@@ -104,16 +104,16 @@ type PathItem struct {
 	// in the defined object and the referenced object, the behavior is undefined.
 	Ref         string      `json:"$ref"`
 	Description string      `json:"description,omitempty"`
-	Get         *Operation  `json:"get"`
-	Put         *Operation  `json:"put"`
-	Post        *Operation  `json:"post"`
-	Delete      *Operation  `json:"delete"`
-	Options     *Operation  `json:"options"`
-	Head        *Operation  `json:"head"`
-	Patch       *Operation  `json:"patch"`
-	Trace       *Operation  `json:"trace"`
-	Servers     []Server    `json:"servers"`
-	Parameters  []Parameter `json:"parameters"`
+	Get         *Operation  `json:"get,omitempty"`
+	Put         *Operation  `json:"put,omitempty"`
+	Post        *Operation  `json:"post,omitempty"`
+	Delete      *Operation  `json:"delete,omitempty"`
+	Options     *Operation  `json:"options,omitempty"`
+	Head        *Operation  `json:"head,omitempty"`
+	Patch       *Operation  `json:"patch,omitempty"`
+	Trace       *Operation  `json:"trace,omitempty"`
+	Servers     []Server    `json:"servers,omitempty"`
+	Parameters  []Parameter `json:"parameters,omitempty"`
 }
 
 // Operation describes a single API operation on a path.
@@ -122,32 +122,32 @@ type Operation struct {
 	// Tags can be used for logical grouping of operations by resources or any other qualifier.
 	Tags        []string     `json:"tags,omitempty"`
 	Description string       `json:"description,omitempty"`
-	OperationID string       `json:"operationId"`
-	Parameters  []Parameter  `json:"parameters"`
-	RequestBody *RequestBody `json:"requestBody"`
-	Responses   Responses    `json:"responses"`
+	OperationID string       `json:"operationId,omitempty"`
+	Parameters  []Parameter  `json:"parameters,omitempty"`
+	RequestBody *RequestBody `json:"requestBody,omitempty"`
+	Responses   Responses    `json:"responses,omitempty"`
 }
 
 // Parameter describes a single operation parameter.
 // A unique parameter is defined by a combination of a name and location.
 type Parameter struct {
-	Ref  string `json:"$ref"`
+	Ref  string `json:"$ref,omitempty"`
 	Name string `json:"name"`
 
 	// The location of the parameter. Possible values are "query", "header", "path" or "cookie".
 	In          string `json:"in"`
-	Description string `json:"description"`
+	Description string `json:"description,omitempty"`
 	Schema      Schema `json:"schema"`
 
 	// Determines whether this parameter is mandatory.
 	// If the parameter location is "path", this property is REQUIRED
 	// and its value MUST be true.
 	// Otherwise, the property MAY be included and its default value is false.
-	Required bool `json:"required"`
+	Required bool `json:"required,omitempty"`
 
 	// Specifies that a parameter is deprecated and SHOULD be transitioned out of usage.
 	// Default value is false.
-	Deprecated bool `json:"deprecated"` // TODO: implement
+	Deprecated bool `json:"deprecated,omitempty"` // TODO: implement
 
 	// For more complex scenarios, the content property can define the media type and schema of the parameter.
 	// A parameter MUST contain either a schema property, or a content property, but not both.
@@ -157,32 +157,32 @@ type Parameter struct {
 	// A map containing the representations for the parameter.
 	// The key is the media type and the value describes it.
 	// The map MUST only contain one entry.
-	Content map[string]Media `json:"content"` // TODO: implement
+	Content map[string]Media `json:"content,omitempty"` // TODO: implement
 
 	// Describes how the parameter value will be serialized
 	// depending on the type of the parameter value.
-	Style string `json:"style"`
+	Style string `json:"style,omitempty"`
 
 	// When this is true, parameter values of type array or object
 	// generate separate parameters for each value of the array
 	// or key-value pair of the map.
 	// For other types of parameters this property has no effect.
-	Explode *bool `json:"explode"`
+	Explode *bool `json:"explode,omitempty"`
 }
 
 // RequestBody describes a single request body.
 type RequestBody struct {
-	Ref         string `json:"$ref"`
-	Description string `json:"description"`
+	Ref         string `json:"$ref,omitempty"`
+	Description string `json:"description,omitempty"`
 
 	// The content of the request body.
 	// The key is a media type or media type range and the value describes it.
 	// For requests that match multiple keys, only the most specific key is applicable.
 	// e.g. text/plain overrides text/*
-	Content map[string]Media `json:"content"`
+	Content map[string]Media `json:"content,omitempty"`
 
 	// Determines if the request body is required in the request. Defaults to false.
-	Required bool `json:"required"`
+	Required bool `json:"required,omitempty"`
 }
 
 // Responses is a container for the expected responses of an operation.
@@ -192,11 +192,11 @@ type Responses map[string]Response
 // Response describes a single response from an API Operation,
 // including design-time, static links to operations based on the response.
 type Response struct {
-	Ref         string                 `json:"$ref"`
-	Description string                 `json:"description"`
-	Header      map[string]interface{} // TODO: implement
-	Content     map[string]Media       `json:"content"`
-	Links       map[string]interface{} // TODO: implement
+	Ref         string                 `json:"$ref,omitempty"`
+	Description string                 `json:"description,omitempty"`
+	Header      map[string]interface{} `json:"header,omitempty"` // TODO: implement
+	Content     map[string]Media       `json:"content,omitempty"`
+	Links       map[string]interface{} `json:"links,omitempty"` // TODO: implement
 }
 
 // Media provides schema and examples for the media type identified by its key.
@@ -208,8 +208,8 @@ type Media struct {
 // The Schema Object allows the definition of input and output data types.
 // These types can be objects, but also primitives and arrays.
 type Schema struct {
-	Ref         string `json:"$ref"`
-	Description string `json:"description"`
+	Ref         string `json:"$ref,omitempty"`
+	Description string `json:"description,omitempty"`
 
 	// Value MUST be a string. Multiple types via an array are not supported.
 	Type string `json:"type"`
@@ -217,20 +217,20 @@ type Schema struct {
 	// See Data Type Formats for further details (https://swagger.io/specification/#data-type-format).
 	// While relying on JSON Schema's defined formats,
 	// the OAS offers a few additional predefined formats.
-	Format string `json:"format"`
+	Format string `json:"format,omitempty"`
 
 	// Property definitions MUST be a Schema Object and not a standard JSON Schema
 	// (inline or referenced).
-	Properties map[string]Schema `json:"properties"`
+	Properties map[string]Schema `json:"properties,omitempty"`
 
 	// The value of this keyword MUST be an array.
 	// This array MUST have at least one element.
 	// Elements of this array MUST be strings, and MUST be unique.
-	Required []string `json:"required"`
+	Required []string `json:"required,omitempty"`
 
 	// Value MUST be an object and not an array.
 	// Inline or referenced schema MUST be of a Schema Object and not a standard
-	Items *Schema `json:"items"`
+	Items *Schema `json:"items,omitempty"`
 
 	// A true value adds "null" to the allowed type specified by the type keyword,
 	// only if type is explicitly defined within the same Schema Object.
@@ -238,30 +238,30 @@ type Schema struct {
 	// and therefore may disallow the use of null as a value.
 	// A false value leaves the specified or default type unmodified.
 	// The default value is false.
-	Nullable bool `json:"nullable"`
+	Nullable bool `json:"nullable,omitempty"`
 
 	// AllOf takes an array of object definitions that are used
 	// for independent validation but together compose a single object.
 	// Still, it does not imply a hierarchy between the models.
 	// For that purpose, you should include the discriminator.
-	AllOf []Schema `json:"allOf"` // TODO: implement.
+	AllOf []Schema `json:"allOf,omitempty"` // TODO: implement.
 
 	// OneOf validates the value against exactly one of the subschemas
-	OneOf []Schema `json:"oneOf"` // TODO: implement.
+	OneOf []Schema `json:"oneOf,omitempty"` // TODO: implement.
 
 	// AnyOf validates the value against any (one or more) of the subschemas
-	AnyOf []Schema `json:"anyOf"` // TODO: implement.
+	AnyOf []Schema `json:"anyOf,omitempty"` // TODO: implement.
 
 	// The value of this keyword MUST be an array.
 	// This array SHOULD have at least one element.
 	// Elements in the array SHOULD be unique.
-	Enum []json.RawMessage `json:"enum"` // TODO: Nullable.
+	Enum []json.RawMessage `json:"enum,omitempty"` // TODO: Nullable.
 
 	// The value of "multipleOf" MUST be a number, strictly greater than 0.
 	//
 	// A numeric instance is only valid if division by this keyword's value
 	// results in an integer.
-	MultipleOf *int `json:"multipleOf"`
+	MultipleOf *int `json:"multipleOf,omitempty"`
 
 	// The value of "maximum" MUST be a number, representing an upper limit
 	// for a numeric instance.
@@ -270,7 +270,7 @@ type Schema struct {
 	// "exclusiveMaximum" is true and instance is less than the provided
 	// value, or else if the instance is less than or exactly equal to the
 	// provided value.
-	Maximum *int64 `json:"maximum"`
+	Maximum *int64 `json:"maximum,omitempty"`
 
 	// The value of "exclusiveMaximum" MUST be a boolean, representing
 	// whether the limit in "maximum" is exclusive or not.  An undefined
@@ -280,7 +280,7 @@ type Schema struct {
 	// equal to the value specified in "maximum".  If "exclusiveMaximum" is
 	// false (or not specified), then a numeric instance MAY be equal to the
 	// value of "maximum".
-	ExclusiveMaximum bool `json:"exclusiveMaximum"`
+	ExclusiveMaximum bool `json:"exclusiveMaximum,omitempty"`
 
 	// The value of "minimum" MUST be a number, representing a lower limit
 	// for a numeric instance.
@@ -289,7 +289,7 @@ type Schema struct {
 	// "exclusiveMinimum" is true and instance is greater than the provided
 	// value, or else if the instance is greater than or exactly equal to
 	// the provided value.
-	Minimum *int64 `json:"minimum"`
+	Minimum *int64 `json:"minimum,omitempty"`
 
 	// The value of "exclusiveMinimum" MUST be a boolean, representing
 	// whether the limit in "minimum" is exclusive or not.  An undefined
@@ -299,7 +299,7 @@ type Schema struct {
 	// equal to the value specified in "minimum".  If "exclusiveMinimum" is
 	// false (or not specified), then a numeric instance MAY be equal to the
 	// value of "minimum".
-	ExclusiveMinimum bool `json:"exclusiveMinimum"`
+	ExclusiveMinimum bool `json:"exclusiveMinimum,omitempty"`
 
 	// The value of this keyword MUST be a non-negative integer.
 	//
@@ -311,7 +311,7 @@ type Schema struct {
 	//
 	// The length of a string instance is defined as the number of its
 	// characters as defined by RFC 7159 [RFC7159].
-	MaxLength *uint64 `json:"maxLength"`
+	MaxLength *uint64 `json:"maxLength,omitempty"`
 
 	// A string instance is valid against this keyword if its length is
 	// greater than, or equal to, the value of this keyword.
@@ -324,7 +324,7 @@ type Schema struct {
 	//
 	// "minLength", if absent, may be considered as being present with
 	// integer value 0.
-	MinLength *int64 `json:"minLength"`
+	MinLength *int64 `json:"minLength,omitempty"`
 
 	// The value of this keyword MUST be a string.  This string SHOULD be a
 	// valid regular expression, according to the ECMA 262 regular
@@ -334,14 +334,14 @@ type Schema struct {
 	// matches the instance successfully.  Recall: regular expressions are
 	// not implicitly anchored.
 	// TODO: implement.
-	Pattern string `json:"pattern"`
+	Pattern string `json:"pattern,omitempty"`
 
 	// The value of this keyword MUST be an integer.  This integer MUST be
 	// greater than, or equal to, 0.
 	//
 	// An array instance is valid against "maxItems" if its size is less
 	// than, or equal to, the value of this keyword.
-	MaxItems *uint64 `json:"maxItems"`
+	MaxItems *uint64 `json:"maxItems,omitempty"`
 
 	// The value of this keyword MUST be an integer.  This integer MUST be
 	// greater than, or equal to, 0.
@@ -351,7 +351,7 @@ type Schema struct {
 	//
 	// If this keyword is not present, it may be considered present with a
 	// value of 0.
-	MinItems *uint64 `json:"minItems"`
+	MinItems *uint64 `json:"minItems,omitempty"`
 
 	// The value of this keyword MUST be a boolean.
 	//
@@ -361,14 +361,14 @@ type Schema struct {
 	//
 	// If not present, this keyword may be considered present with boolean
 	// value false.
-	UniqueItems bool `json:"uniqueItems"`
+	UniqueItems bool `json:"uniqueItems,omitempty"`
 
 	// The value of this keyword MUST be an integer.  This integer MUST be
 	// greater than, or equal to, 0.
 	//
 	// An object instance is valid against "maxProperties" if its number of
 	// properties is less than, or equal to, the value of this keyword.
-	MaxProperties *uint64 `json:"maxProperties"`
+	MaxProperties *uint64 `json:"maxProperties,omitempty"`
 
 	// The value of this keyword MUST be an integer.  This integer MUST be
 	// greater than, or equal to, 0.
@@ -378,5 +378,5 @@ type Schema struct {
 	//
 	// If this keyword is not present, it may be considered present with a
 	// value of 0.
-	MinProperties *uint64 `json:"minProperties"`
+	MinProperties *uint64 `json:"minProperties,omitempty"`
 }
