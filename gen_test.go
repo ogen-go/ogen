9909e7f2ad653b26028f3e2e4ae2125acac840f0
Aleksandr Razumov <ernado@ya.ru>
2021-10-24 14:47:36 +0300

    chore: update telegram testdata

--- gen_test.go
+++ gen_test.go
@@ -60,6 +60,12 @@ func TestGenerate(t *testing.T) {
 			},
 		},
 		{
+			Name: "gotd_bot_api.json",
+			Options: gen.Options{
+				IgnoreNotImplemented: []string{"anyOf"},
+			},
+		},
+		{
 			// https://github.com/kubernetes/kubernetes/tree/master/api/openapi-spec
 			// Generated from OpenAPI v2 (swagger) spec.
 			Name: "k8s.json",
