8be3464807214bdd32e698a353b34623250b0bea
Aleksandr Razumov <ernado@ya.ru>
2021-10-27 11:00:20 +0300

    chore: disable error suppression for firecracker

--- examples/generate.go
+++ examples/generate.go
@@ -4,7 +4,7 @@ import _ "golang.org/x/xerrors"
 
 //go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/api.github.com.json --target ex_github --clean --debug.noerr
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/k8s.json --target ex_k8s --clean --debug.noerr
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/firecracker.json --target ex_firecracker --clean --debug.noerr
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/firecracker.json --target ex_firecracker --clean
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/gotd_bot_api.json --target ex_gotd --clean
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/telegram_bot_api.json --target ex_telegram --clean --debug.noerr
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/nh.json --target ex_nh --clean
