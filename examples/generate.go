f45492d4193ebd17255882e1c305b14fe1b590db
rs <yuiop1955@mail.ru>
2021-10-29 15:12:12 +0500

    chore(examples): enable github schema gen

--- examples/generate.go
+++ examples/generate.go
@@ -2,7 +2,7 @@ package examples
 
 import _ "golang.org/x/xerrors"
 
-//go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/api.github.com.json --target ex_github --clean --debug.noerr
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/api.github.com.json --target ex_github --clean --debug.noerr
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/k8s.json --target ex_k8s --clean --debug.noerr
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/firecracker.json --target ex_firecracker --clean
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/gotd_bot_api.json --target ex_gotd --clean
