e0f1c779733d0c3d886a9bc5c6f5781b42afa6c0
rs <yuiop1955@mail.ru>
2021-11-24 19:26:39 +0500

    chore(uri): add docs

--- uri/interface.go
+++ uri/interface.go
@@ -1,5 +1,20 @@
 package uri
 
+// OpenAPI v3 allows to use only primitive arrays
+// and objects with primitive fields as parameter types.
+//
+// Сurrent Encoder/Decoder interface design allows
+// to represent type with any nesting level.
+// This was done for simplicity of templates.
+//
+// The actual encoders/decoders (PathEncoder, PathDecoder, QueryEncoder, etc)
+// does not support nested types and panic if you try to encode/decode them.
+//
+// To prevent these panics, gen checks that parameter type is satisfying
+// for OpenAPI constraints (internal/gen/gen_parameters.go:isParamAllowed).
+// This should protect from calling interface methods which can panic at runtime.
+// But it still looks pretty dangerous, probably should be rewritten later.
+
 type Encoder interface {
 	EncodeValue(v string) error
 	EncodeArray(f func(e Encoder) error) error
