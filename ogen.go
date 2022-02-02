b9d2572ce14013c3f212b2765e839942e182e3c5
tdakkota <tanc13@yandex.ru>
2022-02-02 09:05:43 +0300

    chore: infer types for sample_api and GitHub API schemas

--- ogen.go
+++ ogen.go
@@ -1,6 +1,6 @@
 // Package ogen implements OpenAPI v3 code generation.
 package ogen
 
-//go:generate go run ./cmd/ogen --schema _testdata/sample.json --target internal/sample_api --clean
+//go:generate go run ./cmd/ogen --schema _testdata/sample.json --target internal/sample_api --infer-types --clean
 //go:generate go run ./cmd/ogen --schema _testdata/errors.json --target internal/sample_err --clean
 //go:generate go run ./cmd/ogen --schema _testdata/techempower.json --target internal/techempower --package techempower --clean
