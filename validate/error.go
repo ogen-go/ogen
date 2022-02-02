2c1ebcc1b0272017811c3059c0ce36493661fbc7
tdakkota <tanc13@yandex.ru>
2022-02-02 14:48:16 +0300

    refactor(gen): use typed errors, add some wrapping

--- validate/error.go
+++ validate/error.go
@@ -1,6 +1,7 @@
 package validate
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/go-faster/errors"
@@ -36,4 +37,41 @@ func (e *Error) Error() string {
 type FieldError struct {
 	Name  string
 	Error error
+}
+
+// ErrBodyRequired reports that request body is required but server got empty request.
+var ErrBodyRequired = errors.New("body required")
+
+// InvalidContentTypeError reports that decoder got unexpected content type.
+type InvalidContentTypeError struct {
+	ContentType string
+}
+
+// InvalidContentTypeError implements error.
+func (i *InvalidContentTypeError) Error() string {
+	return fmt.Sprintf("unexpected Content-Type: %s", i.ContentType)
+}
+
+// InvalidContentType creates new InvalidContentTypeError.
+func InvalidContentType(contentType string) error {
+	return &InvalidContentTypeError{
+		ContentType: contentType,
+	}
+}
+
+// UnexpectedStatusCodeError reports that client got unexpected status code.
+type UnexpectedStatusCodeError struct {
+	StatusCode int
+}
+
+// UnexpectedStatusCode creates new UnexpectedStatusCode.
+func UnexpectedStatusCode(statusCode int) error {
+	return &UnexpectedStatusCodeError{
+		StatusCode: statusCode,
+	}
+}
+
+// UnexpectedStatusCodeError implements error.
+func (i *UnexpectedStatusCodeError) Error() string {
+	return fmt.Sprintf("unexpected status code: %d", i.StatusCode)
 }
