75bfd694e2f767855863b47c260febfebd3bf77a
tdakkota <tanc13@yandex.ru>
2022-02-08 13:07:54 +0300

    test(gen): add regression test for single operation convenient errors

--- ogen.go
+++ ogen.go
@@ -2,5 +2,6 @@
 package ogen
 
 //go:generate go run ./cmd/ogen --schema _testdata/sample.json --target internal/sample_api --infer-types --clean
+//go:generate go run ./cmd/ogen --schema _testdata/test_single_endpoint.json --target internal/test_single_endpoint  --clean
 //go:generate go run ./cmd/ogen --schema _testdata/errors.json --target internal/sample_err --clean
 //go:generate go run ./cmd/ogen --schema _testdata/techempower.json --target internal/techempower --package techempower --clean
