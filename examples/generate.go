73c34b779c5587ef0484d74c855d2be0b9070108
tdakkota <tanc13@yandex.ru>
2022-02-26 15:04:38 +0300

    chore: generate tests for examples

--- examples/generate.go
+++ examples/generate.go
@@ -12,19 +12,19 @@ import (
 
 // Fully supported:
 
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/test_format.json --target ex_test_format --clean
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/test_format.json --target ex_test_format --clean --generate-tests
 
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/petstore.yaml --target ex_petstore --clean
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/firecracker.json --target ex_firecracker --clean
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/gotd_bot_api.json --target ex_gotd --clean
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/tinkoff.json --target ex_tinkoff --clean
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/ent.json --target ex_ent --clean
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/ex_route_params.json --target ex_route_params --clean
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/petstore.yaml --target ex_petstore --clean --generate-tests
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/firecracker.json --target ex_firecracker --clean --generate-tests
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/gotd_bot_api.json --target ex_gotd --clean --generate-tests
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/tinkoff.json --target ex_tinkoff --clean --generate-tests
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/ent.json --target ex_ent --clean --generate-tests
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/ex_route_params.json --target ex_route_params --clean --generate-tests
 
 // Partially supported:
 
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/manga.json --target ex_manga --clean --debug.noerr "unsupported content types"
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/petstore-expanded.yaml --target ex_petstore_expanded --clean --debug.noerr
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/k8s.json --target ex_k8s --clean --debug.noerr
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/manga.json --target ex_manga --clean --debug.noerr "unsupported content types" --generate-tests
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/petstore-expanded.yaml --target ex_petstore_expanded --clean --debug.noerr --generate-tests
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/k8s.json --target ex_k8s --clean --debug.noerr --generate-tests
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/api.github.com.json --target ex_github --clean --infer-types --debug.noerr --generate-tests
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/telegram_bot_api.json --target ex_telegram --clean --debug.noerr
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/telegram_bot_api.json --target ex_telegram --clean --debug.noerr --generate-tests
