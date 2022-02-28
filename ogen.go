c8bd4cce6f6637d596e5c7f797a114e07016c6b0
tdakkota <tanc13@yandex.ru>
2022-02-28 22:01:29 +0300

    chore: generate tests for internal schemas

--- ogen.go
+++ ogen.go
@@ -1,9 +1,9 @@
 // Package ogen implements OpenAPI v3 code generation.
 package ogen
 
-//go:generate go run ./cmd/ogen --schema _testdata/positive/sample.json --target internal/sample_api --infer-types --clean
-//go:generate go run ./cmd/ogen --schema _testdata/positive/test_single_endpoint.json --target internal/test_single_endpoint  --clean
-//go:generate go run ./cmd/ogen --schema _testdata/positive/test_http_responses.json --target internal/test_http_responses  --clean
+//go:generate go run ./cmd/ogen --schema _testdata/positive/sample.json --target internal/sample_api --infer-types --clean --generate-tests
+//go:generate go run ./cmd/ogen --schema _testdata/positive/test_single_endpoint.json --target internal/test_single_endpoint  --clean --generate-tests
+//go:generate go run ./cmd/ogen --schema _testdata/positive/test_http_responses.json --target internal/test_http_responses  --clean --generate-tests
 
-//go:generate go run ./cmd/ogen --schema _testdata/positive/errors.json --target internal/sample_err --clean
-//go:generate go run ./cmd/ogen --schema _testdata/positive/techempower.json --target internal/techempower --package techempower --clean
+//go:generate go run ./cmd/ogen --schema _testdata/positive/errors.json --target internal/sample_err --clean --generate-tests
+//go:generate go run ./cmd/ogen --schema _testdata/positive/techempower.json --target internal/techempower --package techempower --clean --generate-tests
