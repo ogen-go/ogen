367dcc52ac5d11f447e78cf85a8745eb9a5565a7
tdakkota <tanc13@yandex.ru>
2022-03-04 16:19:21 +0300

    chore: update generation options for specs with securities

--- examples/generate.go
+++ examples/generate.go
@@ -17,7 +17,6 @@ import (
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/petstore.yaml --target ex_petstore --clean --generate-tests
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/firecracker.json --target ex_firecracker --clean --generate-tests
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/gotd_bot_api.json --target ex_gotd --clean --generate-tests
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/tinkoff.json --target ex_tinkoff --clean --generate-tests
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/ent.json --target ex_ent --clean --generate-tests
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/ex_route_params.json --target ex_route_params --clean --generate-tests
 
@@ -28,3 +27,4 @@ import (
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/k8s.json --target ex_k8s --clean --debug.noerr --generate-tests
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/api.github.com.json --target ex_github --clean --infer-types --debug.noerr --generate-tests
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/telegram_bot_api.json --target ex_telegram --clean --debug.noerr --generate-tests
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema ../_testdata/positive/tinkoff.json --target ex_tinkoff --clean --debug.noerr "http security" --generate-tests
