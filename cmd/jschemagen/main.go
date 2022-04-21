ef271207c3f86dccebf7d10472a0cca5c3d11471
tdakkota <tanc13@yandex.ru>
2022-04-21 21:18:10 +0300

    feat(gen): add option to trim reference prefixes for name generation

--- cmd/jschemagen/main.go
+++ cmd/jschemagen/main.go
@@ -4,38 +4,87 @@ import (
 	"flag"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"github.com/go-faster/errors"
 
 	"github.com/ogen-go/ogen/gen"
 	"github.com/ogen-go/ogen/gen/genfs"
+	"github.com/ogen-go/ogen/json"
+	"github.com/ogen-go/ogen/jsonschema"
 )
 
-func main() {
+// StringArrayFlag is a string array flag.
+type StringArrayFlag []string
+
+// String implements fmt.Stringer.
+func (i *StringArrayFlag) String() string {
+	return strings.Join(*i, ",")
+}
+
+// Set implements flag.Value.
+func (i *StringArrayFlag) Set(value string) error {
+	*i = append(*i, strings.Split(value, ",")...)
+	return nil
+}
+
+func run() error {
 	var (
 		specPath      = flag.String("schema", "", "Path to openapi spec file")
 		targetFile    = flag.String("target", "output.go", "Path to target")
 		packageName   = flag.String("package", os.Getenv("GOPACKAGE"), "Target package name")
 		typeName      = flag.String("typename", "", "Root schema type name")
-		performFormat = flag.Bool("format", true, "perform code formatting")
+		performFormat = flag.Bool("format", true, "Perform code formatting")
+
+		trimPrefixes StringArrayFlag
 	)
+	flag.Var(&trimPrefixes, "trim-prefixes", "Ref prefixes to trim")
 
 	flag.Parse()
 	if *specPath == "" {
-		panic("no spec provided")
+		return errors.New("no spec provided")
 	}
+
 	data, err := os.ReadFile(*specPath)
 	if err != nil {
-		panic(err)
+		return errors.Wrap(err, "read file")
+	}
+
+	var rawSchema *jsonschema.RawSchema
+	if err := json.Unmarshal(data, &rawSchema); err != nil {
+		return errors.Wrap(err, "unmarshal")
+	}
+	p := jsonschema.NewParser(jsonschema.Settings{
+		Resolver: jsonschema.NewRootResolver(data),
+	})
+	schema, err := p.Parse(rawSchema)
+	if err != nil {
+		return errors.Wrap(err, "parse")
 	}
 
 	dir, file := filepath.Split(filepath.Clean(*targetFile))
 	if err := os.MkdirAll(dir, 0o750); err != nil {
-		panic(err)
+		return errors.Wrap(err, "create output directory")
 	}
 	fs := genfs.FormattedSource{
 		Root:   dir,
 		Format: *performFormat,
 	}
-	if err := gen.GenerateSchema(data, fs, *typeName, file, *packageName); err != nil {
+
+	if err := gen.GenerateSchema(schema, fs, gen.GenerateSchemaOptions{
+		TypeName:   *typeName,
+		FileName:   file,
+		PkgName:    *packageName,
+		TrimPrefix: trimPrefixes,
+	}); err != nil {
+		return errors.Wrap(err, "generate")
+	}
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
 		panic(err)
 	}
 }
