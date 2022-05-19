6b38237d2de8b4bce001cca2baa70e64c7d207d7
tdakkota <tanc13@yandex.ru>
2022-05-19 16:07:16 +0300

    chore: update go:generate comments due to ogen refactor

--- ogen.go
+++ ogen.go
@@ -1,9 +1,9 @@
 // Package ogen implements OpenAPI v3 code generation.
 package ogen
 
-//go:generate go run ./cmd/ogen --schema _testdata/positive/sample.json --target internal/sample_api --infer-types --clean --generate-tests --debug.noerr "enum format"
-//go:generate go run ./cmd/ogen --schema _testdata/positive/test_single_endpoint.json --target internal/test_single_endpoint  --clean --generate-tests
-//go:generate go run ./cmd/ogen --schema _testdata/positive/test_http_responses.json --target internal/test_http_responses  --clean --generate-tests
+//go:generate go run ./cmd/ogen --target internal/sample_api --debug.ignoreNotImplemented "enum format" --infer-types --clean --generate-tests _testdata/positive/sample.json
+//go:generate go run ./cmd/ogen --target internal/test_single_endpoint --clean --generate-tests _testdata/positive/test_single_endpoint.json
+//go:generate go run ./cmd/ogen --target internal/test_http_responses --clean --generate-tests _testdata/positive/test_http_responses.json
 
-//go:generate go run ./cmd/ogen --schema _testdata/positive/errors.json --target internal/sample_err --clean --generate-tests
-//go:generate go run ./cmd/ogen --schema _testdata/positive/techempower.json --target internal/techempower --package techempower --clean --generate-tests
+//go:generate go run ./cmd/ogen --target internal/sample_err --clean --generate-tests _testdata/positive/errors.json
+//go:generate go run ./cmd/ogen --target internal/techempower --package techempower --clean --generate-tests _testdata/positive/techempower.json
