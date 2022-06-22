c2b12883dfd4140faf109fde7cb4aa7d07d1fc78
tdakkota <tanc13@yandex.ru>
2022-06-22 18:40:35 +0300

    feat(ogen): add line and column to parse error

--- parse.go
+++ parse.go
@@ -1,30 +1,44 @@
 package ogen
 
 import (
+	"bytes"
 	"encoding/json"
 
-	"github.com/ghodss/yaml"
 	"github.com/go-faster/errors"
 	"github.com/go-faster/jx"
+	"gopkg.in/yaml.v3"
 )
 
+func wrapLineOffset(data []byte, err error) error {
+	loc, ok := errors.Into[*json.UnmarshalTypeError](err)
+	if !ok {
+		return err
+	}
+
+	if loc.Offset < 0 || int64(len(data)) <= loc.Offset {
+		return err
+	}
+
+	lines := data[:loc.Offset]
+	// Lines count from 1.
+	line := bytes.Count(lines, []byte("\n")) + 1
+	lastNL := int64(bytes.LastIndexByte(lines, '\n'))
+	column := loc.Offset - lastNL
+
+	return errors.Wrapf(err, "line %d:%d", line, column)
+}
+
 // Parse parses JSON/YAML into OpenAPI Spec.
-func Parse(data []byte) (*Spec, error) {
+func Parse(data []byte) (s *Spec, err error) {
+	s = &Spec{}
 	if !jx.Valid(data) {
-		d, err := yaml.YAMLToJSON(data)
-		if err != nil {
-			return nil, err
+		if err := yaml.Unmarshal(data, s); err != nil {
+			return nil, errors.Wrap(err, "yaml")
 		}
-		data = d
+	} else {
+		if err := json.Unmarshal(data, s); err != nil {
+			return nil, errors.Wrap(wrapLineOffset(data, err), "json")
+		}
 	}
-	if len(data) == 0 {
-		return nil, errors.New("blank data")
-	}
-
-	s := &Spec{}
-	if err := json.Unmarshal(data, s); err != nil {
-		return nil, err
-	}
-
 	return s, nil
 }
---
+++ parse_test.go
@@ -0,0 +1,49 @@
+package ogen
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_wrapLineOffset(t *testing.T) {
+	tests := []struct {
+		input        string
+		target       func([]byte) error
+		line, offset int
+	}{
+		{"{\n\t\"a\":1,\n\t\"b\":2\n}", func(input []byte) error {
+			var target struct {
+				A int  `json:"a"`
+				B bool `json:"b"`
+			}
+			return json.Unmarshal(input, &target)
+		}, 3, 7},
+
+		{"[\n0,\ntrue\n]", func(input []byte) error {
+			var target []int
+			return json.Unmarshal(input, &target)
+		}, 3, 5},
+	}
+	for i, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("Test%d", i+1), func(t *testing.T) {
+			input := []byte(tt.input)
+
+			a := require.New(t)
+			err := tt.target(input)
+			a.Error(err)
+
+			err = wrapLineOffset(input, err)
+			a.Error(err)
+
+			msg := err.Error()
+			prefix := fmt.Sprintf("line %d:%d", tt.line, tt.offset)
+			a.Truef(strings.HasPrefix(msg, prefix), "prefix: %s, msg: %s", prefix, msg)
+		})
+	}
+
+}
