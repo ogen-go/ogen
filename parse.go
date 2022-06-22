3f77e5ceaed1efd6a450b9795f8d46d938d80814
tdakkota <tanc13@yandex.ru>
2022-06-22 18:51:02 +0300

    fix(ogen): yaml parsing

--- parse.go
+++ parse.go
@@ -1,32 +1,12 @@
 package ogen
 
 import (
-	"bytes"
 	"encoding/json"
 
+	"github.com/ghodss/yaml"
 	"github.com/go-faster/errors"
 	"github.com/go-faster/jx"
-	"gopkg.in/yaml.v3"
 )
-
-func wrapLineOffset(data []byte, err error) error {
-	loc, ok := errors.Into[*json.UnmarshalTypeError](err)
-	if !ok {
-		return err
-	}
-
-	if loc.Offset < 0 || int64(len(data)) <= loc.Offset {
-		return err
-	}
-
-	lines := data[:loc.Offset]
-	// Lines count from 1.
-	line := bytes.Count(lines, []byte("\n")) + 1
-	lastNL := int64(bytes.LastIndexByte(lines, '\n'))
-	column := loc.Offset - lastNL
-
-	return errors.Wrapf(err, "line %d:%d", line, column)
-}
 
 // Parse parses JSON/YAML into OpenAPI Spec.
 func Parse(data []byte) (s *Spec, err error) {
