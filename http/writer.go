cc36182e86f79e8c7b2fd1ff882d01dcac9ec403
tdakkota <tanc13@yandex.ru>
2022-07-01 04:55:43 +0300

    fix(gen): set GetBody only if client can read body multiple times
    Do not set GetBody for `multipart/form-data`.

--- http/writer.go
+++ http/writer.go
@@ -8,29 +8,25 @@ import (
 )
 
 // CreateMultipartBody is helper for streaming multipart/form-data.
-func CreateMultipartBody(cb func(mw *multipart.Writer) error) (
-	getBody func() (io.ReadCloser, error),
-	contentType string,
-) {
-	r, w := io.Pipe()
-	mw := multipart.NewWriter(w)
-	getBody = func() (io.ReadCloser, error) {
-		wg := new(errgroup.Group)
-		wg.Go(func() (rerr error) {
-			defer func() {
-				_ = mw.Close()
-				_ = w.Close()
-			}()
-			return cb(mw)
-		})
-		return bodyReader{
-			mw: mw,
-			r:  r,
-			w:  w,
-			wg: wg,
-		}, nil
-	}
-	return getBody, mw.FormDataContentType()
+func CreateMultipartBody(cb func(mw *multipart.Writer) error) (io.ReadCloser, string) {
+	piper, pipew := io.Pipe()
+
+	mw := multipart.NewWriter(pipew)
+	wg := new(errgroup.Group)
+	wg.Go(func() (rerr error) {
+		defer func() {
+			_ = mw.Close()
+			_ = pipew.Close()
+		}()
+		return cb(mw)
+	})
+
+	return bodyReader{
+		mw: mw,
+		r:  piper,
+		w:  pipew,
+		wg: wg,
+	}, mw.FormDataContentType()
 }
 
 type bodyReader struct {
