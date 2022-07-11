66a7ec1695f33cfc4e9170ddb550396c1033a44c
tdakkota <tanc13@yandex.ru>
2022-07-11 10:00:04 +0300

    refactor: move `LocationError` to `ogenjson`

--- openapi/parser/errors.go
+++ openapi/parser/errors.go
@@ -1,41 +1,11 @@
 package parser
 
 import (
-	"fmt"
-
-	"github.com/go-faster/errors"
-
 	ogenjson "github.com/ogen-go/ogen/json"
 )
 
-var _ interface {
-	error
-	errors.Wrapper
-	errors.Formatter
-} = (*LocationError)(nil)
-
 // LocationError is a wrapper for an error that has a location.
-type LocationError struct {
-	file string
-	loc  ogenjson.Location
-	err  error
-}
-
-// Unwrap implements errors.Wrapper.
-func (e *LocationError) Unwrap() error {
-	return e.err
-}
-
-// FormatError implements errors.Formatter.
-func (e *LocationError) FormatError(p errors.Printer) (next error) {
-	p.Printf("at %s", e.loc.WithFilename(e.file))
-	return e.err
-}
-
-// Error implements error.
-func (e *LocationError) Error() string {
-	return fmt.Sprintf("at %s: %s", e.loc.WithFilename(e.file), e.err)
-}
+type LocationError = ogenjson.LocationError
 
 func (p *parser) wrapLocation(l ogenjson.Locatable, err error) error {
 	if err == nil || l == nil || p == nil {
@@ -46,8 +16,8 @@ func (p *parser) wrapLocation(l ogenjson.Locatable, err error) error {
 		return err
 	}
 	return &LocationError{
-		file: p.filename,
-		loc:  loc,
-		err:  err,
+		File: p.filename,
+		Loc:  loc,
+		Err:  err,
 	}
 }
