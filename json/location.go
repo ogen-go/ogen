02c47154fc70cf900d34e9c463cc747a504482c3
tdakkota <tanc13@yandex.ru>
2022-07-11 14:12:34 +0300

    feat(json): add helpers to return field/array element location

--- json/location.go
+++ json/location.go
@@ -110,8 +110,67 @@ func (l Lines) LineColumn(offset int64) (line, column int64, ok bool) {
 
 // Location is a JSON value location.
 type Location struct {
-	Line, Column int64
+	Line, Column int
 	Node         *yaml.Node
+}
+
+func (l *Location) fromNode(node *yaml.Node) {
+	*l = Location{
+		Line:   node.Line,
+		Column: node.Column,
+		Node:   node,
+	}
+}
+
+// Field tries to find the child node using given key and returns its location.
+// If such node is not found or parent node is not a mapping, Field returns location of the parent node.
+//
+// NOTE: child location will point to the value node, not to the key node.
+func (l Location) Field(key string) (loc Location) {
+	n := l.Node
+	if n != nil && n.Kind == yaml.DocumentNode {
+		if len(n.Content) < 1 {
+			return l
+		}
+		n = n.Content[0]
+	}
+
+	if n == nil || n.Kind != yaml.MappingNode || len(n.Content) < 2 {
+		return l
+	}
+
+	children := n.Content
+	for i := 0; i < len(children); i += 2 {
+		keyNode, valueNode := children[i], children[i+1]
+		if keyNode.Value == key {
+			loc.fromNode(valueNode)
+			return loc
+		}
+	}
+	return l
+}
+
+// Index tries to find the child node using given index and returns its location.
+// If such node is not found or parent node is not a sequence, Field returns location of the parent node.
+func (l Location) Index(idx int) (loc Location) {
+	n := l.Node
+	if n != nil && n.Kind == yaml.DocumentNode {
+		if len(n.Content) < 1 {
+			return l
+		}
+		n = n.Content[0]
+	}
+
+	if n == nil || n.Kind != yaml.SequenceNode {
+		return l
+	}
+
+	children := n.Content
+	if idx < 0 || idx >= len(children) {
+		return l
+	}
+	loc.fromNode(children[idx])
+	return loc
 }
 
 // String implements fmt.Stringer.
@@ -159,6 +218,26 @@ func (l Locator) Location() (Location, bool) {
 	return l.location, l.set
 }
 
+// Field tries to find the child node using given key and returns its location.
+//
+// See Field method of Location.
+func (l Locator) Field(key string) (loc Locator) {
+	if l.set {
+		loc.SetLocation(l.location.Field(key))
+	}
+	return
+}
+
+// Index tries to find the child node using given index and returns its location.
+//
+// See Index method of Location.
+func (l Locator) Index(idx int) (loc Locator) {
+	if l.set {
+		loc.SetLocation(l.location.Index(idx))
+	}
+	return
+}
+
 // MarshalYAML implements yaml.Marshaler.
 func (l *Locator) MarshalYAML(n *yaml.Node) error {
 	return nil
@@ -166,10 +245,8 @@ func (l *Locator) MarshalYAML(n *yaml.Node) error {
 
 // UnmarshalYAML implements yaml.Unmarshaler.
 func (l *Locator) UnmarshalYAML(n *yaml.Node) error {
-	l.SetLocation(Location{
-		Line:   int64(n.Line),
-		Column: int64(n.Column),
-		Node:   n,
-	})
+	var loc Location
+	loc.fromNode(n)
+	l.SetLocation(loc)
 	return nil
 }
