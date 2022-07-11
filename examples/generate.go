659c9107a38af257db44a554b38ac3de33a94bb8
rs <shepper116@gmail.com>
2022-07-12 02:43:40 +0500

    chore: format generate.go files again

--- examples/generate.go
+++ examples/generate.go
@@ -16,19 +16,19 @@ import (
 
 // Fully supported:
 //
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_test_format ../_testdata/positive/format_gen.json
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_petstore ../_testdata/examples/petstore.yml
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_firecracker ../_testdata/examples/firecracker.json
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_gotd ../_testdata/examples/gotd_bot_api.json
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_ent ../_testdata/examples/ent.json
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_route_params ../_testdata/positive/ex_route_params.json
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_manga ../_testdata/examples/manga.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_test_format       ../_testdata/positive/format_gen.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_petstore          ../_testdata/examples/petstore.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_firecracker       ../_testdata/examples/firecracker.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_gotd              ../_testdata/examples/gotd_bot_api.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_ent               ../_testdata/examples/ent.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_route_params      ../_testdata/positive/ex_route_params.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_manga             ../_testdata/examples/manga.json
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_petstore_expanded ../_testdata/examples/petstore-expanded.yml
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_telegram ../_testdata/examples/telegram_bot_api.json
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_2ch ../_testdata/examples/2ch.yml
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_tinkoff ../_testdata/examples/tinkoff.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_telegram          ../_testdata/examples/telegram_bot_api.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_2ch               ../_testdata/examples/2ch.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_tinkoff           ../_testdata/examples/tinkoff.json
 
 // Partially supported:
 //
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --debug.noerr --generate-tests --target ex_k8s  ../_testdata/examples/k8s.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --debug.noerr --generate-tests               --target ex_k8s    ../_testdata/examples/k8s.json
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --debug.noerr --generate-tests --infer-types --target ex_github ../_testdata/examples/api.github.com.json
--- internal/generate.go
+++ internal/generate.go
@@ -1,17 +1,20 @@
 package internal
 
 // Sample API matrix:
-//go:generate go run ../cmd/ogen -v --debug.ignoreNotImplemented "enum format" --infer-types --clean --target sample_api --generate-tests ../_testdata/positive/sample.json
-//go:generate go run ../cmd/ogen -v --debug.ignoreNotImplemented "enum format" --infer-types --clean --no-server --target sample_api_ns ../_testdata/positive/sample.json
-//go:generate go run ../cmd/ogen -v --debug.ignoreNotImplemented "enum format" --infer-types --clean --no-client --target sample_api_nc ../_testdata/positive/sample.json
+//
+//go:generate go run ../cmd/ogen -v --debug.ignoreNotImplemented "enum format" --infer-types --clean --generate-tests        --target sample_api      ../_testdata/positive/sample.json
+//go:generate go run ../cmd/ogen -v --debug.ignoreNotImplemented "enum format" --infer-types --clean --no-server             --target sample_api_ns   ../_testdata/positive/sample.json
+//go:generate go run ../cmd/ogen -v --debug.ignoreNotImplemented "enum format" --infer-types --clean --no-client             --target sample_api_nc   ../_testdata/positive/sample.json
 //go:generate go run ../cmd/ogen -v --debug.ignoreNotImplemented "enum format" --infer-types --clean --no-server --no-client --target sample_api_nsnc ../_testdata/positive/sample.json
 
 //go:generate go run ../cmd/ogen -v --clean --target sample_err ../_testdata/positive/errors.json
 //go:generate go run ../cmd/ogen -v --clean --package techempower --generate-tests --target techempower ../_testdata/examples/techempower.json
 
 // Tests
+//
 //go:generate go run ../cmd/ogen -v --clean --target test_single_endpoint ../_testdata/positive/single_endpoint.json
-//go:generate go run ../cmd/ogen -v --clean --target test_http_responses ../_testdata/positive/http_responses.json
-//go:generate go run ../cmd/ogen -v --clean --target test_http_requests ../_testdata/positive/http_requests.json
-//go:generate go run ../cmd/ogen -v --clean --target test_form ../_testdata/positive/form.json
+//go:generate go run ../cmd/ogen -v --clean --target test_http_responses  ../_testdata/positive/http_responses.json
+//go:generate go run ../cmd/ogen -v --clean --target test_http_requests   ../_testdata/positive/http_requests.json
+//go:generate go run ../cmd/ogen -v --clean --target test_form            ../_testdata/positive/form.json
+//
 //go:generate go run ../cmd/ogen -v --clean --generate-tests --target test_allof ../_testdata/positive/allof.yml
