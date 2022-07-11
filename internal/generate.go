05fcb7f3c34a87e70816b21b03678ffe656a77a4
rs <shepper116@gmail.com>
2022-07-12 02:20:06 +0500

    chore(internal): cleanup generate.go file

--- internal/generate.go
+++ internal/generate.go
@@ -1,17 +1,17 @@
 package internal
 
 // Sample API matrix:
-//go:generate go run ../cmd/ogen -v --target sample_api --debug.ignoreNotImplemented "enum format" --infer-types --clean --generate-tests ../_testdata/positive/sample.json
-//go:generate go run ../cmd/ogen -v --no-server --target sample_api_ns --debug.ignoreNotImplemented "enum format" --infer-types --clean ../_testdata/positive/sample.json
-//go:generate go run ../cmd/ogen -v --no-client --target sample_api_nc --debug.ignoreNotImplemented "enum format" --infer-types --clean ../_testdata/positive/sample.json
-//go:generate go run ../cmd/ogen -v --no-server --no-client --target sample_api_nsnc --debug.ignoreNotImplemented "enum format" --infer-types --clean ../_testdata/positive/sample.json
+//go:generate go run ../cmd/ogen -v --debug.ignoreNotImplemented "enum format" --infer-types --clean --target sample_api --generate-tests ../_testdata/positive/sample.json
+//go:generate go run ../cmd/ogen -v --debug.ignoreNotImplemented "enum format" --infer-types --clean --no-server --target sample_api_ns ../_testdata/positive/sample.json
+//go:generate go run ../cmd/ogen -v --debug.ignoreNotImplemented "enum format" --infer-types --clean --no-client --target sample_api_nc ../_testdata/positive/sample.json
+//go:generate go run ../cmd/ogen -v --debug.ignoreNotImplemented "enum format" --infer-types --clean --no-server --no-client --target sample_api_nsnc ../_testdata/positive/sample.json
 
-//go:generate go run ../cmd/ogen -v --target sample_err --clean ../_testdata/positive/errors.json
-//go:generate go run ../cmd/ogen -v --target techempower --package techempower --clean --generate-tests ../_testdata/examples/techempower.json
+//go:generate go run ../cmd/ogen -v --clean --target sample_err ../_testdata/positive/errors.json
+//go:generate go run ../cmd/ogen -v --clean --package techempower --generate-tests --target techempower ../_testdata/examples/techempower.json
 
 // Tests
-//go:generate go run ../cmd/ogen -v --target test_single_endpoint --clean ../_testdata/positive/single_endpoint.json
-//go:generate go run ../cmd/ogen -v --target test_http_responses --clean ../_testdata/positive/http_responses.json
-//go:generate go run ../cmd/ogen -v --target test_http_requests --clean ../_testdata/positive/http_requests.json
-//go:generate go run ../cmd/ogen -v --target test_form --clean ../_testdata/positive/form.json
-//go:generate go run ../cmd/ogen -v --target test_allof --clean --generate-tests ../_testdata/positive/allof.yml
+//go:generate go run ../cmd/ogen -v --clean --target test_single_endpoint ../_testdata/positive/single_endpoint.json
+//go:generate go run ../cmd/ogen -v --clean --target test_http_responses ../_testdata/positive/http_responses.json
+//go:generate go run ../cmd/ogen -v --clean --target test_http_requests ../_testdata/positive/http_requests.json
+//go:generate go run ../cmd/ogen -v --clean --target test_form ../_testdata/positive/form.json
+//go:generate go run ../cmd/ogen -v --clean --generate-tests --target test_allof ../_testdata/positive/allof.yml
