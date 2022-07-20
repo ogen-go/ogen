fcde111d2c261a69697f7cf8f28da0197d5a66c0
tdakkota <tanc13@yandex.ru>
2022-07-20 19:19:53 +0300

    test: add status code pattern tests

--- internal/responses_test.go
+++ internal/responses_test.go
@@ -3,7 +3,9 @@ package internal_test
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"io"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
@@ -55,114 +57,298 @@ func (t testHTTPResponses) TextPlainBinaryStringSchema(ctx context.Context) (api
 	}, nil
 }
 
+func (t testHTTPResponses) IntersectPatternCode(ctx context.Context, params api.IntersectPatternCodeParams) (api.IntersectPatternCodeRes, error) {
+	if params.Code == 200 {
+		var resp api.IntersectPatternCodeOKApplicationJSON = "200"
+		return &resp, nil
+	}
+	return &api.IntersectPatternCode2XXStatusCode{
+		StatusCode: params.Code,
+		Response:   params.Code,
+	}, nil
+}
+
+func (t testHTTPResponses) Combined(ctx context.Context, params api.CombinedParams) (api.CombinedRes, error) {
+	switch params.Type {
+	case api.CombinedType200:
+		return &api.CombinedOK{
+			Ok: "200",
+		}, nil
+	case api.CombinedType2XX:
+		return &api.Combined2XXStatusCode{
+			StatusCode: http.StatusAccepted,
+			Response:   http.StatusAccepted,
+		}, nil
+	case api.CombinedType5XX:
+		return &api.Combined5XXStatusCode{
+			StatusCode: http.StatusInternalServerError,
+			Response:   true,
+		}, nil
+	case api.CombinedTypeDefault:
+		return &api.CombinedDefStatusCode{
+			StatusCode: http.StatusNotFound,
+			Response:   []string{"default"},
+		}, nil
+	default:
+		panic(fmt.Sprintf("unknown type %q", params.Type))
+	}
+}
+
 func (t testHTTPResponses) Headers200(ctx context.Context) (api.Headers200OK, error) {
 	return api.Headers200OK{
 		TestHeader: "foo",
 	}, nil
 }
 
-func (t testHTTPResponses) HeadersCombined(ctx context.Context, params api.HeadersCombinedParams) (api.HeadersCombinedRes, error) {
-	if params.Default {
-		return &api.HeadersCombinedDef{
-			StatusCode: 202,
-			TestHeader: "default=true",
-		}, nil
-	}
-	return &api.HeadersCombinedOK{
-		TestHeader: "default=false",
-	}, nil
-}
-
 func (t testHTTPResponses) HeadersDefault(ctx context.Context) (api.HeadersDefaultDef, error) {
 	return api.HeadersDefaultDef{
-		StatusCode: 202,
 		TestHeader: "202",
+		StatusCode: 202,
 	}, nil
 }
 
-func TestResponses(t *testing.T) {
-	a := require.New(t)
-	ctx := context.Background()
+func (t testHTTPResponses) HeadersPattern(ctx context.Context) (api.HeadersPattern4XX, error) {
+	return api.HeadersPattern4XX{
+		TestHeader: "404",
+		StatusCode: 404,
+	}, nil
+}
 
-	testData := []byte("bababoi")
+func (t testHTTPResponses) HeadersCombined(ctx context.Context, params api.HeadersCombinedParams) (api.HeadersCombinedRes, error) {
+	switch params.Type {
+	case api.HeadersCombinedType200:
+		return &api.HeadersCombinedOK{
+			TestHeader: "200",
+		}, nil
+	case api.HeadersCombinedTypeDefault:
+		return &api.HeadersCombinedDef{
+			TestHeader: "default",
+			StatusCode: 202,
+		}, nil
+	case api.HeadersCombinedType4XX:
+		return &api.HeadersCombined4XX{
+			TestHeader: "4XX",
+			StatusCode: 404,
+		}, nil
+	default:
+		panic(fmt.Sprintf("unknown type %q", params.Type))
+	}
+}
+
+func testResponsesInit(t *testing.T, testData []byte) (*require.Assertions, *api.Client) {
+	a := require.New(t)
+
 	srv, err := api.NewServer(testHTTPResponses{
 		data: testData,
 	})
 	a.NoError(err)
 
 	s := httptest.NewServer(srv)
-	defer s.Close()
+	t.Cleanup(func() {
+		s.Close()
+	})
 
 	client, err := api.NewClient(s.URL, api.WithClient(s.Client()))
 	a.NoError(err)
 
-	{
+	return a, client
+}
+
+func TestResponsesEncoding(t *testing.T) {
+	testData := []byte("bababoi")
+	create := func(t *testing.T) (context.Context, *require.Assertions, *api.Client) {
+		a, client := testResponsesInit(t, testData)
+		return context.Background(), a, client
+	}
+
+	t.Run("AnyContentTypeBinaryStringSchema", func(t *testing.T) {
+		ctx, a, client := create(t)
+
 		r, err := client.AnyContentTypeBinaryStringSchema(ctx)
 		a.NoError(err)
 		data, err := io.ReadAll(r.Data)
 		a.NoError(err)
 		a.Equal(testData, data)
-	}
-	{
+	})
+	t.Run("AnyContentTypeBinaryStringSchemaDefault", func(t *testing.T) {
+		ctx, a, client := create(t)
+
 		r, err := client.AnyContentTypeBinaryStringSchemaDefault(ctx)
 		a.NoError(err)
 		data, err := io.ReadAll(r.Response.Data)
 		a.NoError(err)
 		a.Equal(testData, data)
 		a.Equal(200, r.StatusCode)
-	}
-	{
+	})
+	t.Run("MultipleGenericResponses", func(t *testing.T) {
+		ctx, a, client := create(t)
+
 		r, err := client.MultipleGenericResponses(ctx)
 		a.NoError(err)
 		a.Equal(&api.NilString{Null: true}, r)
-	}
-	{
+	})
+	t.Run("OctetStreamBinaryStringSchema", func(t *testing.T) {
+		ctx, a, client := create(t)
+
 		r, err := client.OctetStreamBinaryStringSchema(ctx)
 		a.NoError(err)
 		data, err := io.ReadAll(r.Data)
 		a.NoError(err)
 		a.Equal(testData, data)
-	}
-	{
+	})
+	t.Run("OctetStreamEmptySchema", func(t *testing.T) {
+		ctx, a, client := create(t)
+
 		r, err := client.OctetStreamEmptySchema(ctx)
 		a.NoError(err)
 		data, err := io.ReadAll(r.Data)
 		a.NoError(err)
 		a.Equal(testData, data)
-	}
-	{
+	})
+	t.Run("TextPlainBinaryStringSchema", func(t *testing.T) {
+		ctx, a, client := create(t)
+
 		r, err := client.TextPlainBinaryStringSchema(ctx)
 		a.NoError(err)
 		data, err := io.ReadAll(r.Data)
 		a.NoError(err)
 		a.Equal(testData, data)
-	}
-	{
-		{
-			r, err := client.Headers200(ctx)
-			a.NoError(err)
-			a.Equal(r.TestHeader, "foo")
-		}
-		{
-			r, err := client.HeadersDefault(ctx)
-			a.NoError(err)
-			a.Equal(r.StatusCode, 202)
-			a.Equal(r.TestHeader, "202")
-		}
-		{
-			r, err := client.HeadersCombined(ctx, api.HeadersCombinedParams{
-				Default: false,
-			})
-			a.NoError(err)
-			a.IsType(r, &api.HeadersCombinedOK{})
-			a.Equal(r.(*api.HeadersCombinedOK).TestHeader, "default=false")
+	})
+}
 
-			r, err = client.HeadersCombined(ctx, api.HeadersCombinedParams{
-				Default: true,
-			})
-			a.NoError(err)
-			a.IsType(r, &api.HeadersCombinedDef{})
-			a.Equal(r.(*api.HeadersCombinedDef).TestHeader, "default=true")
-		}
+func TestResponsesHeaders(t *testing.T) {
+	testData := []byte("bababoi")
+	create := func(t *testing.T) (context.Context, *require.Assertions, *api.Client) {
+		a, client := testResponsesInit(t, testData)
+		return context.Background(), a, client
 	}
+
+	t.Run("Headers200", func(t *testing.T) {
+		ctx, a, client := create(t)
+
+		r, err := client.Headers200(ctx)
+		a.NoError(err)
+		a.Equal(r.TestHeader, "foo")
+	})
+	t.Run("HeadersDefault", func(t *testing.T) {
+		ctx, a, client := create(t)
+
+		r, err := client.HeadersDefault(ctx)
+		a.NoError(err)
+		a.Equal(r.StatusCode, 202)
+		a.Equal(r.TestHeader, "202")
+	})
+	t.Run("HeadersPattern", func(t *testing.T) {
+		ctx, a, client := create(t)
+
+		r, err := client.HeadersPattern(ctx)
+		a.NoError(err)
+		a.Equal(r.StatusCode, 404)
+		a.Equal(r.TestHeader, "404")
+	})
+	t.Run("HeadersCombined", func(t *testing.T) {
+		tests := []struct {
+			Param    api.HeadersCombinedType
+			Response api.HeadersCombinedRes
+		}{
+			{
+				api.HeadersCombinedType200,
+				&api.HeadersCombinedOK{TestHeader: "200"},
+			},
+			{
+				api.HeadersCombinedTypeDefault,
+				&api.HeadersCombinedDef{TestHeader: "default", StatusCode: 202},
+			},
+			{
+				api.HeadersCombinedType4XX,
+				&api.HeadersCombined4XX{TestHeader: "4XX", StatusCode: 404},
+			},
+		}
+		for _, tt := range tests {
+			tt := tt
+			t.Run(string(tt.Param), func(t *testing.T) {
+				ctx, a, client := create(t)
+
+				r, err := client.HeadersCombined(ctx, api.HeadersCombinedParams{
+					Type: tt.Param,
+				})
+				a.NoError(err, tt.Param)
+				a.Equal(tt.Response, r, tt.Param)
+			})
+		}
+	})
+}
+
+func TestResponsesPattern(t *testing.T) {
+	testData := []byte("bababoi")
+	create := func(t *testing.T) (context.Context, *require.Assertions, *api.Client) {
+		a, client := testResponsesInit(t, testData)
+		return context.Background(), a, client
+	}
+
+	t.Run("IntersectPatternCode", func(t *testing.T) {
+		r200 := api.IntersectPatternCodeOKApplicationJSON("200")
+		tests := []struct {
+			Code int
+			Type api.IntersectPatternCodeRes
+		}{
+			{200, &r200},
+			{201, &api.IntersectPatternCode2XXStatusCode{
+				StatusCode: 201,
+				Response:   201,
+			}},
+		}
+		for _, tt := range tests {
+			tt := tt
+			t.Run(fmt.Sprintf("Code%d", tt.Code), func(t *testing.T) {
+				ctx, a, client := create(t)
+
+				r, err := client.IntersectPatternCode(ctx, api.IntersectPatternCodeParams{Code: tt.Code})
+				a.NoError(err)
+				a.Equal(tt.Type, r)
+			})
+		}
+	})
+	t.Run("Combined", func(t *testing.T) {
+		tests := []struct {
+			Param api.CombinedType
+			Type  api.CombinedRes
+		}{
+			{
+				api.CombinedType200,
+				&api.CombinedOK{Ok: "200"},
+			},
+			{
+				api.CombinedType2XX,
+				&api.Combined2XXStatusCode{
+					StatusCode: http.StatusAccepted,
+					Response:   http.StatusAccepted,
+				},
+			},
+			{
+				api.CombinedType5XX,
+				&api.Combined5XXStatusCode{
+					StatusCode: http.StatusInternalServerError,
+					Response:   true,
+				},
+			},
+			{
+				api.CombinedTypeDefault,
+				&api.CombinedDefStatusCode{
+					StatusCode: http.StatusNotFound,
+					Response:   []string{"default"},
+				},
+			},
+		}
+		for _, tt := range tests {
+			tt := tt
+			t.Run(string(tt.Param), func(t *testing.T) {
+				ctx, a, client := create(t)
+
+				r, err := client.Combined(ctx, api.CombinedParams{Type: tt.Param})
+				a.NoError(err)
+				a.Equal(tt.Type, r)
+			})
+		}
+	})
 }
