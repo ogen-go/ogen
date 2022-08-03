4af3a1b4b39438915125dca7a9dc76a53b6630c8
Aleksandr Razumov <ernado@ya.ru>
2022-08-03 10:32:25 +0300

    chore(gen): update

--- examples/ex_github/oas_schemas_gen.go
+++ examples/ex_github/oas_schemas_gen.go
@@ -33500,7 +33500,9 @@ type PullsCreateReviewCommentReq struct {
 
 // **Required with `comfort-fade` preview**. In a split diff view, the side of the diff that the pull
 // request's changes appear on. Can be `LEFT` or `RIGHT`. Use `LEFT` for deletions that appear in red.
-//  Use `RIGHT` for additions that appear in green or unchanged lines that appear in white and are
+//
+//	Use `RIGHT` for additions that appear in green or unchanged lines that appear in white and are
+//
 // shown for context. For a multi-line comment, side represents whether the last line of the comment
 // range is a deletion or addition. For more information, see "[Diff view options](https://help.
 // github.com/en/articles/about-comparing-branches-in-pull-requests#diff-view-options)" in the GitHub
