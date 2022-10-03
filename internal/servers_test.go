ef68f8d8b943162a798a87ec6fc38fbc74776cfc
tdakkota <tanc13@yandex.ru>
2022-10-03 13:28:57 +0300

    test: add servers generation test

---
+++ internal/servers_test.go
@@ -0,0 +1,48 @@
+package internal_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	api "github.com/ogen-go/ogen/internal/test_servers"
+)
+
+func TestServers(t *testing.T) {
+	t.Run("Template", func(t *testing.T) {
+		a := require.New(t)
+		s := api.ProductionServer{}
+
+		r := s.MustBuild()
+		a.Equal("https://us.example.com/prod/v1", r)
+		r = s.MustPath()
+		a.Equal("/prod/v1", r)
+
+		s.Region = "eu"
+		r = s.MustBuild()
+		a.Equal("https://eu.example.com/prod/v1", r)
+		r = s.MustPath()
+		a.Equal("/prod/v1", r)
+
+		s.Region = "ru"
+		_, err := s.Build()
+		a.Error(err)
+		a.Panics(func() {
+			s.MustBuild()
+		})
+		_, err = s.Path()
+		a.Error(err)
+		a.Panics(func() {
+			s.MustPath()
+		})
+	})
+	t.Run("Const", func(t *testing.T) {
+		a := require.New(t)
+		s := api.ConstServer
+
+		r := s.MustBuild()
+		a.Equal("https://cdn.example.com/v1", r)
+		r = s.MustPath()
+		a.Equal("/v1", r)
+	})
+}
