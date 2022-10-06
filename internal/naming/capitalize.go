6a3fbc15b6ecc4724b77514fe6ce743dd3b28e37
tdakkota <tanc13@yandex.ru>
2022-10-06 14:02:41 +0300

    fix(naming): do not try to capitalize invalid or empty string

--- internal/naming/capitalize.go
+++ internal/naming/capitalize.go
@@ -6,7 +6,12 @@ import (
 )
 
 // Capitalize converts first character to upper.
+//
+// If the string is invalid UTF-8 or empty, it is returned as is.
 func Capitalize(s string) string {
 	r, size := utf8.DecodeRuneInString(s)
+	if r == utf8.RuneError {
+		return s
+	}
 	return string(unicode.ToUpper(r)) + s[size:]
 }
