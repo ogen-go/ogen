7d994a9a9fe97384bbd00e69411c20fcfa951aa4
tdakkota <tanc13@yandex.ru>
2022-10-13 01:12:51 +0300

    feat(otelogen): add webhook name key

--- otelogen/tracer.go
+++ otelogen/tracer.go
@@ -7,12 +7,22 @@ import (
 const (
 	// OperationIDKey by OpenAPI specification.
 	OperationIDKey = attribute.Key("oas.operation")
+	// WebhookNameKey by OpenAPI specification.
+	WebhookNameKey = attribute.Key("oas.webhook.name")
 )
 
 // OperationID attribute.
 func OperationID(v string) attribute.KeyValue {
 	return attribute.KeyValue{
 		Key:   OperationIDKey,
+		Value: attribute.StringValue(v),
+	}
+}
+
+// WebhookName attribute.
+func WebhookName(v string) attribute.KeyValue {
+	return attribute.KeyValue{
+		Key:   WebhookNameKey,
 		Value: attribute.StringValue(v),
 	}
 }
