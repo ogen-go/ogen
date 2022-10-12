2eee6d6bf69ca2b34bf8a9f84a8b9cc9df99c333
tdakkota <tanc13@yandex.ru>
2022-10-12 20:23:43 +0300

    feat(parser): pass webhook locator

--- openapi/parser/parse_webhook.go
+++ openapi/parser/parse_webhook.go
@@ -18,6 +18,7 @@ func (p *parser) parseWebhook(name string, item *ogen.PathItem, ctx *jsonpointer
 	return openapi.Webhook{
 		Name:       name,
 		Operations: pi,
+		Locator:    item.Common.Locator,
 	}, nil
 }
 
