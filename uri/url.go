82581ec5afc995d2addd4722326274e90bb7084e
tdakkota <tanc13@yandex.ru>
2023-03-15 13:22:26 +0300

    feat(url): add `AddPathParts` function

--- uri/url.go
+++ uri/url.go
@@ -2,6 +2,7 @@ package uri
 
 import (
 	"net/url"
+	"strings"
 )
 
 // Clone clones u and returns cloned url that you can modify.
@@ -17,4 +18,41 @@ func Clone(u *url.URL) *url.URL {
 	target.User = u.User
 
 	return target
+}
+
+// AddPathParts adds escaped path parts to the given URL.
+func AddPathParts(u *url.URL, parts ...string) {
+	var (
+		path    strings.Builder
+		rawPath strings.Builder
+
+		writeRaw = u.RawPath != ""
+	)
+	path.WriteString(u.Path)
+	if writeRaw {
+		rawPath.WriteString(u.RawPath)
+	}
+
+	for _, part := range parts {
+		if !strings.ContainsRune(part, '%') {
+			path.WriteString(part)
+			if writeRaw {
+				rawPath.WriteString(part)
+			}
+			continue
+		}
+
+		if !writeRaw {
+			rawPath.WriteString(path.String())
+		}
+		writeRaw = true
+		rawPath.WriteString(part)
+		unescaped, _ := url.PathUnescape(part)
+		path.WriteString(unescaped)
+	}
+
+	u.Path = path.String()
+	if rawPath.Len() > 0 {
+		u.RawPath = rawPath.String()
+	}
 }
--- uri/url_test.go
+++ uri/url_test.go
@@ -1,6 +1,7 @@
 package uri
 
 import (
+	"fmt"
 	"net/url"
 	"testing"
 
@@ -21,5 +22,90 @@ func BenchmarkCloneURL(b *testing.B) {
 
 	if uCloned.Host != "go.dev" {
 		b.Fatal(uCloned)
+	}
+}
+
+func TestAddPathParts(t *testing.T) {
+	const (
+		exampleAPI  = "https://example.com/api/v1"
+		emptyPath   = "https://example.com"
+		escapedBase = "https://example.com/api/v%2F1"
+	)
+	tests := []struct {
+		Root    string
+		Parts   []string
+		Path    string
+		RawPath string
+		URL     string
+	}{
+		// Plain base path.
+		{
+			Root:    exampleAPI,
+			Parts:   []string{"/foo/baz/bar"},
+			Path:    "/api/v1/foo/baz/bar",
+			RawPath: "",
+			URL:     "https://example.com/api/v1/foo/baz/bar",
+		},
+		{
+			Root:    exampleAPI,
+			Parts:   []string{"/repo/", "ogen-go", "/", "ogen", "/issues"},
+			Path:    "/api/v1/repo/ogen-go/ogen/issues",
+			RawPath: "",
+			URL:     "https://example.com/api/v1/repo/ogen-go/ogen/issues",
+		},
+		{
+			Root:    exampleAPI,
+			Parts:   []string{"/repo/", "ogen%25go", "/", "ogen"},
+			Path:    "/api/v1/repo/ogen%go/ogen",
+			RawPath: "/api/v1/repo/ogen%25go/ogen",
+			URL:     "https://example.com/api/v1/repo/ogen%25go/ogen",
+		},
+
+		// Empty base path.
+		{
+			Root:    emptyPath,
+			Parts:   []string{"/repo/", "ogen%25go", "/", "ogen"},
+			Path:    "/repo/ogen%go/ogen",
+			RawPath: "/repo/ogen%25go/ogen",
+			URL:     "https://example.com/repo/ogen%25go/ogen",
+		},
+
+		// Escaped base path.
+		{
+			Root:    escapedBase,
+			Parts:   []string{"/foo/baz/bar"},
+			Path:    "/api/v/1/foo/baz/bar",
+			RawPath: "/api/v%2F1/foo/baz/bar",
+			URL:     "https://example.com/api/v%2F1/foo/baz/bar",
+		},
+		{
+			Root:    escapedBase,
+			Parts:   []string{"/repo/", "ogen-go", "/", "ogen", "/issues"},
+			Path:    "/api/v/1/repo/ogen-go/ogen/issues",
+			RawPath: "/api/v%2F1/repo/ogen-go/ogen/issues",
+			URL:     "https://example.com/api/v%2F1/repo/ogen-go/ogen/issues",
+		},
+		{
+			Root:    escapedBase,
+			Parts:   []string{"/repo/", "ogen%25go", "/", "ogen"},
+			Path:    "/api/v/1/repo/ogen%go/ogen",
+			RawPath: "/api/v%2F1/repo/ogen%25go/ogen",
+			URL:     "https://example.com/api/v%2F1/repo/ogen%25go/ogen",
+		},
+	}
+
+	for i, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("Test%d", i+1), func(t *testing.T) {
+			a := require.New(t)
+
+			u, err := url.Parse(tt.Root)
+			a.NoError(err)
+
+			AddPathParts(u, tt.Parts...)
+			a.Equal(tt.Path, u.Path)
+			a.Equal(tt.RawPath, u.RawPath)
+			a.Equal(tt.URL, u.String())
+		})
 	}
 }
