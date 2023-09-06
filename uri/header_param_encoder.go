ab68ce3edb224ad4e09ab31f228db9dbc68c22ea
James Harris <contact@jamesharris.id.au>
2023-09-06 10:36:44 +1000

    fix: do not fold Set-Cookie values into single header
    This commit adds a special case for encoding Set-Cookie headers in responses.
    Specifically, it prevents `headerParamEncoder` from folding multiple header
    values into a single header value with a comma separator, as per RFC6265:
    
       Origin servers SHOULD NOT fold multiple Set-Cookie header fields into
       a single header field.  The usual mechanism for folding HTTP headers
       fields (i.e., as defined in [RFC2616]) might change the semantics of
       the Set-Cookie header field because the %x2C (",") character is used
       by Set-Cookie in a way that conflicts with such folding.
    
    The comma mentioned by the RFC appears when using a cookie with an expiry
    timestamp, because the timestamp format uses a comma after the day-of-week.

--- uri/header_param_encoder.go
+++ uri/header_param_encoder.go
@@ -20,13 +20,26 @@ func (e *headerParamEncoder) serialize() error {
 		e.header.Set(e.paramName, e.val)
 		return nil
 	case typeArray:
-		const sep = ","
-		for _, val := range e.items {
-			if err := checkNotContains(val, sep); err != nil {
-				return err
+		if strings.EqualFold(e.paramName, "set-cookie") {
+			// As per RFC6265:
+			//
+			// Origin servers SHOULD NOT fold multiple Set-Cookie header fields into
+			// a single header field. The usual mechanism for folding HTTP headers
+			// fields (i.e., as defined in RFC2616) might change the semantics of
+			// the Set-Cookie header field because the %x2C (",") character is used
+			// by Set-Cookie in a way that conflicts with such folding.
+			for _, val := range e.items {
+				e.header.Add(e.paramName, val)
 			}
+		} else {
+			const sep = ","
+			for _, val := range e.items {
+				if err := checkNotContains(val, sep); err != nil {
+					return err
+				}
+			}
+			e.header.Set(e.paramName, strings.Join(e.items, sep))
 		}
-		e.header.Set(e.paramName, strings.Join(e.items, sep))
 		return nil
 	case typeObject:
 		var kvSep, fieldSep byte = ',', ','
