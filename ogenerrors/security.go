923207686faa99a4730f26e413112dac0584a27b
Ales Brelih <ales.brelih@gmail.com>
2023-09-21 10:20:31 +0200

    feat(security): added option to exclude sec requirement
    Added option to exclude security requirement for handlers.

--- ogenerrors/security.go
+++ ogenerrors/security.go
@@ -6,5 +6,7 @@ var (
 	// ErrSecurityRequirementIsNotSatisfied is returned when security requirement is not satisfied.
 	ErrSecurityRequirementIsNotSatisfied = errors.New("security requirement is not satisfied")
 	// ErrSkipClientSecurity is guard error to exclude security scheme from client request.
-	ErrSkipClientSecurity = errors.New("skip security")
+	ErrSkipClientSecurity = errors.New("skip client security")
+	// ErrSkipServerSecurity is guard error to exclude security scheme from server request.
+	ErrSkipServerSecurity = errors.New("skip server security")
 )
