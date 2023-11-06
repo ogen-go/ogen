cedf663894c42c860c34520a81c78a35e11245eb
tdakkota <tanc13@yandex.ru>
2023-11-07 01:55:02 +0300

    chore(examples): add configs for examples generation

--- examples/generate.go
+++ examples/generate.go
@@ -14,21 +14,21 @@ import (
 
 // Fully supported:
 //
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_test_format       ../_testdata/positive/format_gen.json
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_petstore          ../_testdata/examples/petstore.yml
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_firecracker       ../_testdata/examples/firecracker.json
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_gotd              ../_testdata/examples/gotd_bot_api.json
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_ent               ../_testdata/examples/ent.json
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_route_params      ../_testdata/positive/ex_route_params.json
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_manga             ../_testdata/examples/manga.json
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_petstore_expanded ../_testdata/examples/petstore-expanded.yml
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_telegram          ../_testdata/examples/telegram_bot_api.json
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_2ch               ../_testdata/examples/2ch.yml
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --generate-tests --target ex_tinkoff           ../_testdata/examples/tinkoff.json
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --target ex_openai                             ../_testdata/examples/openai.yml
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --target ex_oauth2                             ../_testdata/examples/petstore-oauth2.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --config _config/with_tests.yml --target ex_test_format       ../_testdata/positive/format_gen.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --config _config/with_tests.yml --target ex_petstore          ../_testdata/examples/petstore.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --config _config/with_tests.yml --target ex_firecracker       ../_testdata/examples/firecracker.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --config _config/with_tests.yml --target ex_gotd              ../_testdata/examples/gotd_bot_api.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --config _config/with_tests.yml --target ex_ent               ../_testdata/examples/ent.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --config _config/with_tests.yml --target ex_route_params      ../_testdata/positive/ex_route_params.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --config _config/with_tests.yml --target ex_manga             ../_testdata/examples/manga.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --config _config/with_tests.yml --target ex_petstore_expanded ../_testdata/examples/petstore-expanded.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --config _config/with_tests.yml --target ex_telegram          ../_testdata/examples/telegram_bot_api.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --config _config/with_tests.yml --target ex_2ch               ../_testdata/examples/2ch.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --config _config/with_tests.yml --target ex_tinkoff           ../_testdata/examples/tinkoff.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --target ex_openai                                            ../_testdata/examples/openai.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --target ex_oauth2                                            ../_testdata/examples/petstore-oauth2.yml
 
 // Partially supported:
 //
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --debug.noerr --generate-tests               --target ex_k8s    ../_testdata/examples/k8s.json
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --debug.noerr --generate-tests --infer-types --target ex_github ../_testdata/examples/api.github.com.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --config _config/k8s.yml    --target ex_k8s    ../_testdata/examples/k8s.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --clean --config _config/github.yml --target ex_github ../_testdata/examples/api.github.com.json
