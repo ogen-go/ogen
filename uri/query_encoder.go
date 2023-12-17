8d500c0dc229a430471bde8589a2cdf42148297c
tdakkota <tanc13@yandex.ru>
2023-12-18 01:08:19 +0300

    feat(uri): add special constructor for form encoding

--- uri/query_encoder.go
+++ uri/query_encoder.go
@@ -1,19 +1,32 @@
 package uri
 
 import (
+	"fmt"
+	"io"
 	"mime/multipart"
+	"net/textproto"
 	"net/url"
+	"strings"
 
 	"github.com/go-faster/errors"
 )
 
 type QueryEncoder struct {
 	values url.Values
+
+	ct map[string]string
 }
 
 func NewQueryEncoder() *QueryEncoder {
 	return &QueryEncoder{
 		values: make(url.Values),
+	}
+}
+
+func NewFormEncoder(ct map[string]string) *QueryEncoder {
+	return &QueryEncoder{
+		values: make(url.Values),
+		ct:     ct,
 	}
 }
 
@@ -48,10 +61,32 @@ func (e *QueryEncoder) Values() url.Values {
 func (e *QueryEncoder) WriteMultipart(w *multipart.Writer) error {
 	for k, values := range e.values {
 		for i, value := range values {
-			if err := w.WriteField(k, value); err != nil {
+			if err := e.writeMultipartField(w, k, value); err != nil {
 				return errors.Wrapf(err, "write %q: [%d]", k, i)
 			}
 		}
 	}
 	return nil
+}
+
+func (e *QueryEncoder) writeMultipartField(w *multipart.Writer, key, value string) error {
+	h := make(textproto.MIMEHeader)
+	h.Set("Content-Disposition",
+		fmt.Sprintf(`form-data; name="%s"`, escapeQuotes(key)))
+	if contentType, ok := e.ct[key]; ok {
+		h.Set("Content-Type", contentType)
+	}
+
+	field, err := w.CreatePart(h)
+	if err != nil {
+		return err
+	}
+	_, err = io.WriteString(field, value)
+	return err
+}
+
+var quoteEscaper = strings.NewReplacer("\\", "\\\\", `"`, "\\\"")
+
+func escapeQuotes(s string) string {
+	return quoteEscaper.Replace(s)
 }
