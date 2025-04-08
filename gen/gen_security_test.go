6c358359d5a7312e2661b21c9012680ea90f84a7
Jean Schmitt <github@jeanschmitt.fr>
2025-04-08 08:55:21 +0200

    feat(gen): add scopes for non-oauth securities
    test(integration): fix security comparison

--- gen/gen_security_test.go
+++ gen/gen_security_test.go
@@ -7,7 +7,6 @@ import (
 	"go.uber.org/zap/zaptest"
 
 	"github.com/ogen-go/ogen/gen/ir"
-	"github.com/ogen-go/ogen/location"
 	"github.com/ogen-go/ogen/openapi"
 )
 
@@ -24,32 +23,98 @@ func TestGenerateSecurities(t *testing.T) {
 		securities: map[string]*ir.Security{},
 	}
 
-	securityRequirement1 := openapi.SecurityRequirement{
-		Schemes: []openapi.SecurityScheme{
-			{
-				Name:   "oauth2test",
-				Scopes: []string{"scope1", "scope2"},
-				Security: openapi.Security{
-					Type: "oauth2",
+	securityRequirements := []openapi.SecurityRequirement{
+		{
+			Schemes: []openapi.SecurityScheme{
+				{
+					Name:   "oauth2test",
+					Scopes: []string{"scope1", "scope2"},
+					Security: openapi.Security{
+						Type: "oauth2",
+					},
 				},
 			},
 		},
-		Pointer: location.Pointer{},
-	}
-	securityRequirement2 := openapi.SecurityRequirement{
-		Schemes: []openapi.SecurityScheme{
-			{
-				Name:   "oauth2test",
-				Scopes: []string{"scope3"},
-				Security: openapi.Security{
-					Type: "oauth2",
+		{
+			Schemes: []openapi.SecurityScheme{
+				{
+					Name:   "oauth2test",
+					Scopes: []string{"scope3"},
+					Security: openapi.Security{
+						Type: "oauth2",
+					},
 				},
 			},
 		},
-		Pointer: location.Pointer{},
+		{
+			Schemes: []openapi.SecurityScheme{
+				{
+					Name:   "apiKeyTest",
+					Scopes: []string{"scope4"},
+					Security: openapi.Security{
+						Type: "apiKey",
+						Name: "key",
+						In:   "query",
+					},
+				},
+			},
+		},
+		{
+			Schemes: []openapi.SecurityScheme{
+				{
+					Name:   "basicTest",
+					Scopes: []string{"scope5"},
+					Security: openapi.Security{
+						Type:   "http",
+						Scheme: "basic",
+					},
+				},
+			},
+		},
+		{
+			Schemes: []openapi.SecurityScheme{
+				{
+					Name:   "bearerTest",
+					Scopes: []string{"scope6"},
+					Security: openapi.Security{
+						Type:   "http",
+						Scheme: "bearer",
+					},
+				},
+			},
+		},
 	}
 
-	securityRequirements := []openapi.SecurityRequirement{securityRequirement1, securityRequirement2}
+	wantSecurities := []*ir.Security{
+		{
+			Kind:   ir.HeaderSecurity,
+			Format: ir.Oauth2SecurityFormat,
+			Scopes: map[string][]string{
+				"testOp": {"scope1", "scope2", "scope3"},
+			},
+		},
+		{
+			Kind:   ir.QuerySecurity,
+			Format: ir.APIKeySecurityFormat,
+			Scopes: map[string][]string{
+				"testOp": {"scope4"},
+			},
+		},
+		{
+			Kind:   ir.HeaderSecurity,
+			Format: ir.BasicHTTPSecurityFormat,
+			Scopes: map[string][]string{
+				"testOp": {"scope5"},
+			},
+		},
+		{
+			Kind:   ir.HeaderSecurity,
+			Format: ir.BearerSecurityFormat,
+			Scopes: map[string][]string{
+				"testOp": {"scope6"},
+			},
+		},
+	}
 
 	secRequirements, err := gen.generateSecurities(
 		ctx,
@@ -57,12 +122,15 @@ func TestGenerateSecurities(t *testing.T) {
 		securityRequirements,
 	)
 	require.NoError(t, err)
-	require.Equal(t, len(secRequirements.Securities), 1)
-	require.Equal(t, len(secRequirements.Requirements), 2)
-	sec := secRequirements.Securities[0]
-	require.Equal(t, sec.Kind, ir.HeaderSecurity)
-	require.Equal(t, sec.Format, ir.Oauth2SecurityFormat)
-	operationScopes, ok := sec.Scopes["testOp"]
-	require.True(t, ok)
-	require.EqualValues(t, operationScopes, []string{"scope1", "scope2", "scope3"})
+	require.Len(t, secRequirements.Securities, len(wantSecurities))
+	require.Len(t, secRequirements.Requirements, len(securityRequirements))
+
+	for i, wantSec := range wantSecurities {
+		sec := secRequirements.Securities[i]
+
+		require.Equal(t, wantSec.Kind, sec.Kind)
+		require.Equal(t, wantSec.Format, sec.Format)
+		require.Contains(t, sec.Scopes, "testOp")
+		require.Equal(t, wantSec.Scopes["testOp"], sec.Scopes["testOp"])
+	}
 }
