0054cbd0237ce3997d3f5c8674522818e9245f7a
kukymbr <me@kukymbr.dev>
2025-04-18 11:40:05 +0300

    chore: commit generated files

--- internal/integration/test_parameters/oas_parameters_gen.go
+++ internal/integration/test_parameters/oas_parameters_gen.go
@@ -490,7 +490,7 @@ func decodeContentParametersParams(args [1]string, argsEscaped bool, r *http.Req
 
 // CookieParameterParams is parameters of cookieParameter operation.
 type CookieParameterParams struct {
-	Value string
+	Value string `customTag:"test"`
 }
 
 func unpackCookieParameterParams(packed middleware.Parameters) (params CookieParameterParams) {
@@ -545,7 +545,7 @@ func decodeCookieParameterParams(args [0]string, argsEscaped bool, r *http.Reque
 
 // HeaderParameterParams is parameters of headerParameter operation.
 type HeaderParameterParams struct {
-	XValue string
+	XValue string `customTag:"test"`
 }
 
 func unpackHeaderParameterParams(packed middleware.Parameters) (params HeaderParameterParams) {
@@ -600,7 +600,7 @@ func decodeHeaderParameterParams(args [0]string, argsEscaped bool, r *http.Reque
 
 // ObjectCookieParameterParams is parameters of objectCookieParameter operation.
 type ObjectCookieParameterParams struct {
-	Value OneLevelObject
+	Value OneLevelObject `customTag:"test"`
 }
 
 func unpackObjectCookieParameterParams(packed middleware.Parameters) (params ObjectCookieParameterParams) {
@@ -644,9 +644,9 @@ func decodeObjectCookieParameterParams(args [0]string, argsEscaped bool, r *http
 
 // ObjectQueryParameterParams is parameters of objectQueryParameter operation.
 type ObjectQueryParameterParams struct {
-	FormObject OptOneLevelObject
+	FormObject OptOneLevelObject `customTag:"test"`
 
-	DeepObject OptOneLevelObject
+	DeepObject OptOneLevelObject `customTag:"test"`
 }
 
 func unpackObjectQueryParameterParams(packed middleware.Parameters) (params ObjectQueryParameterParams) {
@@ -740,9 +740,9 @@ func decodeObjectQueryParameterParams(args [0]string, argsEscaped bool, r *http.
 
 // OptionalArrayParameterParams is parameters of optionalArrayParameter operation.
 type OptionalArrayParameterParams struct {
-	Query []string
+	Query []string `customTag:"test"`
 
-	Header []string
+	Header []string `customTag:"test"`
 }
 
 func unpackOptionalArrayParameterParams(packed middleware.Parameters) (params OptionalArrayParameterParams) {
@@ -859,7 +859,7 @@ func decodeOptionalArrayParameterParams(args [0]string, argsEscaped bool, r *htt
 
 // PathParameterParams is parameters of pathParameter operation.
 type PathParameterParams struct {
-	Value string
+	Value string `customTag:"test"`
 }
 
 func unpackPathParameterParams(packed middleware.Parameters) (params PathParameterParams) {
