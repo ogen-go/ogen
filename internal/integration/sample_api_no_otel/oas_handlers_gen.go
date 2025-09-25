39b6b28047400ff1b57e4f5c0e4f0ec43f11108d
Sam Nguyen <118077+dtjm@users.noreply.github.com>
2025-09-25 13:18:48 -0700

    add support for disjoint roles

--- internal/integration/sample_api_no_otel/oas_handlers_gen.go
+++ internal/integration/sample_api_no_otel/oas_handlers_gen.go
@@ -1799,8 +1799,10 @@ func (s *Server) handleSecurityTestRequest(args [0]string, argsEscaped bool, w h
 	{
 		type bitset = [1]uint8
 		var satisfied bitset
+		allRoles := map[string][]string{"SecurityTest": []string{}}
 		{
-			sctx, ok, err := s.securityAPIKey(ctx, SecurityTestOperation, r)
+			roles := allRoles["SecurityTest"]
+			sctx, ok, err := s.securityAPIKey(ctx, SecurityTestOperation, r, roles)
 			if err != nil {
 				err = &ogenerrors.SecurityError{
 					OperationContext: opErrContext,
